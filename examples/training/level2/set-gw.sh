091f6311b552be08359cdd77789dd5af803d401e
Bartek Palak <bartek.palak@man.poznan.pl>
2021-02-05 13:41:24 +0100

    Updating env configuration files

--- examples/training/level2/set-gw.sh
+++ examples/training/level2/set-gw.sh
@@ -1,3 +1,6 @@
+setenv ids_path ""
+setenv PYTHONPATH ""
+
 module purge
 module load cineca
 module load itm-python/3.6
@@ -5,9 +8,8 @@ module load IMAS/3.29.0
 module load xmllib/3.3.1/intel/17.0
 module load pycharm
 
-if (! $?PYTHONPATH) then  
-    setenv PYTHONPATH ""
-endif
+mkdir -p  $PWD/tmp
+setenv ids_path "$PWD/tmp;$ids_path"
 
 setenv PYTHONPATH $PWD/physics_ii/:$PYTHONPATH
 setenv PYTHONPATH $PWD/../../common/python:$PYTHONPATH
--- examples/training/level2/set-iter.sh
+++ examples/training/level2/set-iter.sh
@@ -1,4 +1,12 @@
-module load Python
+export ids_path=""
+export PYTHONPATH=""
 
-setenv PYTHONPATH=$PWD/physics_ii/:$PYTHONPATH
-export PYTHONPATH=$PWD/../common/python:$PYTHONPATH
+module purge
+module load IMAS/3.29.0-4.8.4
+module load  XMLlib/3.3.1-intel-2018a
+
+mkdir -p  $PWD/tmp
+export ids_path="$PWD/tmp;$ids_path"
+
+export PYTHONPATH=$PWD/physics_ii/:$PYTHONPATH
+export PYTHONPATH=$PWD/../../common/python:$PYTHONPATH
