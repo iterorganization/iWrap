78377667317458786981729fa0c75058cffd7dca
Bartek Palak <bartek.palak@man.poznan.pl>
2021-02-05 11:49:13 +0100

    Arguments: adding 'name' and __str__

--- examples/common/python/iwrap/arguments.py
+++ examples/common/python/iwrap/arguments.py
@@ -1,22 +1,18 @@
 
 class Argument :
-    IN = 10
-    OUT = 20
+    IN = 'IN'
+    OUT = 'OUT'
 
-    def __init__(self, arg_type, sub_type, intent):
+    def __init__(self, name, arg_type, sub_type, intent):
+        self.name = name # (INT, DOUBLE, STRING, COMPLEX, IDS) READ ONLY
         self.type = arg_type # (INT, DOUBLE, STRING, COMPLEX, IDS) READ ONLY
         self.sub_type = sub_type  #'equilibrium'  for IDSes only READ ONLY
         self.intent = intent # (IN/OUT) READ ONLY
-        self.value  = 7. # to be set by user
-      
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-class DiagnosticInfo :
-    def __init__(self):
-        self.status = 0
-        self.message = ''
 
-class Parameters : 
-    def __init__(self):
-        self.code_parameters = None # file name or string or... (?)
-        self.default_parameters = None # file name or string or ... (?)
-        self.schema = None # file name or string or ... (?)
+    def __str__(self):
+        str_ = 'Name : ' + self.name + '\n' \
+            + 'Type : ' + self.type + '\n' \
+            + 'Sub-type : ' + self.sub_type + '\n' \
+            + 'Intent : ' + self.intent + '\n'
+        return str_
+
