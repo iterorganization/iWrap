c225711d3f39e4bc7e83dcd9465249a4cc396bea
Bartek Palak <bartek.palak@man.poznan.pl>
2021-03-12 11:02:43 +0100

    "fortran_settings" module added for specific language settings

---
+++ iwrap/settings/language_specific/fortran_settings.py
@@ -0,0 +1,19 @@
+from iwrap.common.misc import Dictionarizable
+
+
+class FortranSpecificSettings(Dictionarizable):
+
+    def __init__(self):
+        self.compiler = ''
+        self.mpi = ''
+        self.open_mp = False
+        self.system_libraries = []
+        self.custom_libraries = []
+
+    def from_dict(self, dictionary: dict):
+        super().from_dict( dictionary )
+
+    def to_dict(self):
+        return super().to_dict()
+
+    pass
