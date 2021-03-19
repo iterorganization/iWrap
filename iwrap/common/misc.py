0754d3af361fa2f71d7fc6c54773d7438a150b18
Bartek Palak <bartek.palak@man.poznan.pl>
2021-03-19 08:45:24 +0100

    Decorator 'classproperty' added

--- iwrap/common/misc.py
+++ iwrap/common/misc.py
@@ -2,6 +2,14 @@ from abc import ABC, abstractmethod
 from typing import List
 
 
+class classproperty( object ):
+
+    def __init__(self, fget):
+        self.fget = fget
+
+    def __get__(self, owner_self, owner_cls):
+        return self.fget( owner_cls )
+
 class Dictionarizable( ABC ):
 
     @abstractmethod
