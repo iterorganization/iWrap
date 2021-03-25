9e280dc319df29b52c4ddd0b19e1bf99d1bff657
Maciej Smukowski <g2msmuk@r054c06s01.galileo.cineca.it>
2021-03-25 10:16:28 +0100

    Added new test example

--- tests/example.py
+++ tests/example.py
@@ -3,3 +3,8 @@
 def example():
     """Return 1 on example purpose"""
     return 1
+
+
+def another_example():
+    """Return string 'Test'"""
+    return "Test"
--- tests/example_test.py
+++ tests/example_test.py
@@ -1,9 +1,14 @@
-from tests.example import example
+from tests.example import example, another_example
 
 
 def test_example() -> None:
     """Testing example method"""
     assert example() == 1
+
+
+def test_another_example() -> None:
+    """Testing another example method"""
+    assert another_example() == "Test"
 
 
 def test_always_passes():
