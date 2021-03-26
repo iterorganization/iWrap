baf33ddfc36979d87f7f9daeff6deacf400cca45
Natalia Kraszewska <g2nkrasz@r054c06s01.galileo.cineca.it>
2021-03-26 18:50:06 +0100

    Fix extensions and html_theme in configuration file

--- docs/conf.py
+++ docs/conf.py
@@ -41,9 +41,9 @@ release = ''
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'sphinx.ext.napoleo',,
+    'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
-    'sphinx.ext.extlink',
+    'sphinx.ext.extlinks',
 ]
 
 autodoc_default_options = {
@@ -55,7 +55,7 @@ autodoc_default_options = {
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -87,7 +87,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme =sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
