81aa2c2265e6b6142fcff85a0742287e612df6d0
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-03-26 16:19:21 +0100

    Fix environment bash script

--- tests/set_test_env.sh
+++ tests/set_test_env.sh
@@ -3,7 +3,7 @@
 echo "--------------Set up ITER modules environment--------------"
 set -e
 # Set up environment
-./usr/share/Modules/init/sh
+. /usr/share/Modules/init/sh
 module use /work/imas/etc/modulefiles
 module use /work/imas/etc/modules/all
 module purge
