56e2875fa799e4d3a99da57798e091f2e5d4a260
Natalia Kraszewska <g2nkrasz@r054c06s01.galileo.cineca.it>
2021-04-02 10:08:12 +0200

    Remove unneeded self, change compiler_combobox to programming_language_combobox

--- iwrap/gui/settings/code_settings_pane.py
+++ iwrap/gui/settings/code_settings_pane.py
@@ -10,24 +10,22 @@ class CodeSettingsPane(ttk.Frame, IWrapPane):
         super().__init__(master)
 
         # LABEL FRAME
-        self.labelframe = ttk.LabelFrame(self, text="User code settings")
-        self.labelframe.pack(fill="both", expand="yes", pady=10)
-        self.labelframe.grid_columnconfigure(1, weight=1)
+        labelframe = ttk.LabelFrame(self, text="User code settings")
+        labelframe.pack(fill="both", expand="yes", pady=10)
+        labelframe.grid_columnconfigure(1, weight=1)
 
         # COMBOBOX
-        ttk.Label(self.labelframe, text="Programming language:").grid(column=0, row=0, padx=10, pady=5,
-                                                                      sticky=(tk.W, tk.N))
-        self.compiler_combobox = ttk.Combobox(self.labelframe, state='readonly')
-        self.compiler_combobox['values'] = (' Fortran', ' C++', ' Python')
-        self.compiler_combobox.grid(column=1, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
+        ttk.Label(labelframe, text="Programming language:").grid(column=0, row=0, padx=10, pady=5, sticky=(tk.W, tk.N))
+        programming_language_combobox = ttk.Combobox(labelframe, state='readonly')
+        programming_language_combobox['values'] = (' Fortran', ' C++', ' Python')
+        programming_language_combobox.grid(column=1, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
 
         # BROWSE BUTTON AND ENTRY FOR PATH
-        ttk.Label(self.labelframe, text="Source Code:").grid(column=0, row=1, padx=10, pady=5, sticky=(tk.W, tk.N))
-        self.browse_text = tk.Entry(self.labelframe, state='disabled')
+        ttk.Label(labelframe, text="Source Code:").grid(column=0, row=1, padx=10, pady=5, sticky=(tk.W, tk.N))
+        self.browse_text = tk.Entry(labelframe)
         self.browse_text.grid(column=1, row=1, padx=10, pady=5, sticky=(tk.W, tk.E))
-        self.button = ttk.Button(self.labelframe, text="Browse", command=self.on_click, width=10)\
+        ttk.Button(labelframe, text="Browse", command=self.on_click, width=10)\
             .grid(row=1, column=2, padx=10, pady=5)
-
 
     def reload(self):
         pass
