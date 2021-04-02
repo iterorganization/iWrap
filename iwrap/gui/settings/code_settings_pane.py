32f360197389affa205cbc45b3d12998648c71db
Natalia Kraszewska <g2nkrasz@r054c06s01.galileo.cineca.it>
2021-04-02 08:14:33 +0200

    Add LabelFrame and Combobox for User code settings pane

--- iwrap/gui/settings/code_settings_pane.py
+++ iwrap/gui/settings/code_settings_pane.py
@@ -1,13 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinter.filedialog
 
 from iwrap.gui.generics import IWrapPane
 
 
-class CodeSettingsPane( ttk.Frame, IWrapPane ):
+class CodeSettingsPane(ttk.Frame, IWrapPane):
     def __init__(self, master=None):
-        super().__init__( master )
-        ttk.Label( self, text="Place for SETTINGS", borderwidth=1, relief="solid" ).pack( fill=tk.BOTH, expand=1 )
+        super().__init__(master)
+
+        # LABEL FRAME
+        self.labelframe = ttk.LabelFrame(self, text="User code settings")
+        self.labelframe.pack(fill="both", expand="yes", pady=10)
+        self.labelframe.grid_columnconfigure(1, weight=3)
+
+        # COMBOBOX
+        ttk.Label(self.labelframe, text="Programming language:").grid(column=0, row=0, padx=10, pady=5)
+        self.compiler_combobox = ttk.Combobox(self.labelframe, state='readonly')
+        self.compiler_combobox['values'] = (' Fortran', ' C++', ' Python')
+        self.compiler_combobox.grid(column=1, columnspan=10, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
 
     def reload(self):
         pass
