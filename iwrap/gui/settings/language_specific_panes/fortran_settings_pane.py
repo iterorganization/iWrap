776777cb697b59c903b4225f21cb5e61336a9423
Natalia Kraszewska <g2nkrasz@r054c07s01.galileo.cineca.it>
2021-04-06 15:12:11 +0200

    Add labelframe and compiler combobox

--- iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
+++ iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
@@ -7,7 +7,18 @@ from iwrap.gui.generics import IWrapPane
 class FortranPane( ttk.Frame, IWrapPane ):
     def __init__(self, master=None):
         super().__init__( master )
-        ttk.Label( self, text="Place for SPECIFIC FORTRAN settings", borderwidth=1, relief="solid" ).pack( fill=tk.BOTH, expand=1 )
+        self.combobox_values = ['compiler1', 'compiler2', 'compiler3']
+
+        # LABEL FRAME
+        labelframe = ttk.LabelFrame(self, text="Language specific settings", borderwidth=2, relief="groove")
+        labelframe.pack(fill=tk.BOTH, expand=1)
+        labelframe.grid_columnconfigure(1, weight=1)
+
+        # COMBOBOX
+        ttk.Label(labelframe, text="Compiler:").grid(column=0, row=0, padx=10, pady=5, sticky=(tk.W, tk.N))
+        self.compiler_combobox = ttk.Combobox(labelframe, state='readonly')
+        self.compiler_combobox['values'] = self.combobox_values
+        self.compiler_combobox.grid(column=1, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
 
     def reload(self):
         pass
