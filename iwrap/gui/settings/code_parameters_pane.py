3bc6fc7a148baa09897e56a9cc71afc53d6697b5
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-04-07 16:34:52 +0200

    Add FileBrowser class

--- iwrap/gui/settings/code_parameters_pane.py
+++ iwrap/gui/settings/code_parameters_pane.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
+from tkinter.constants import S, SEL_FIRST
 
 from iwrap.gui.generics import IWrapPane
 
@@ -7,8 +9,27 @@ from iwrap.gui.generics import IWrapPane
 class CodeParametersPane( ttk.Frame, IWrapPane ):
     def __init__(self, master=None):
         super().__init__( master )
-        ttk.Label( self, text='Place for CODE PARAMETERS', borderwidth=1, relief="solid" ).pack( fill=tk.BOTH,
-                                                                                                 expand=1 )
+
+        # XML file path browser dialog
+        self._xml_browser = FileBrowser(self)
+        
+        # XSD file path browser dialog
+        self._xsd_browser = FileBrowser(self)
+        
 
     def reload(self):
         pass
+
+
+class FileBrowser:
+    def __init__(self, master=None) -> None:
+        self.button = ttk.Button(master, text = "Browse XML File",command = self.action_open)
+        self.button.pack()
+    
+    def action_open(self):
+        filename = filedialog.askopenfilename(  initialdir=None, 
+                                                title="Select XML file",
+                                                filetypes=(("xml files","*.xml"),) )
+        if filename is None:
+            return
+        print(filename)
