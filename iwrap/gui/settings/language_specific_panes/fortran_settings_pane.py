a5335aec658994faf40cf0b8f142a7a9f960090f
Bartek Palak <bartek.palak@man.poznan.pl>
2021-04-07 14:53:14 +0200

    Fortran settings pane: loading/updating values

--- iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
+++ iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
@@ -2,11 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 
 from iwrap.gui.generics import IWrapPane
+from iwrap.settings.language_specific.fortran_settings import FortranSpecificSettings
+from iwrap.settings.project import ProjectSettings
 
 
 class FortranPane( ttk.Frame, IWrapPane ):
     def __init__(self, master=None):
         super().__init__( master )
+
+        self.settings = FortranSpecificSettings()
         self.combobox_values = ['compiler1', 'compiler2', 'compiler3']
 
         # LABEL FRAME
@@ -21,4 +25,11 @@ class FortranPane( ttk.Frame, IWrapPane ):
         self.compiler_combobox.grid(column=1, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
 
     def reload(self):
+        dict_settings = ProjectSettings.get_settings().code_description.language_specific
+        self.settings.from_dict(dict_settings)
+        # TO DO: GUI REFRESH
         pass
+
+    def update_settings(self):
+        dict_settings = self.settings.to_dict( )
+        ProjectSettings.get_settings().code_description.language_specific = dict_settings
