df8b1994b0e07a1d435a96db57d647a38f3c5d87
Bartek Palak <bartek.palak@man.poznan.pl>
2021-04-08 08:51:26 +0200

    Handling errors while loading code description

--- iwrap/common/misc.py
+++ iwrap/common/misc.py
@@ -7,19 +7,21 @@ class Dictionarizable( ABC ):
     @abstractmethod
     def from_dict(self, dictionary: dict):
         for name, value in dictionary.items():
-            if hasattr( self, name ):
-                attr = getattr( self, name )
-                if isinstance( attr, Dictionarizable ):
-                    attr.from_dict( value )
-                else:
-                    setattr( self, name, value )
+            if not hasattr( self, name ):
+                error_msg = f'ERROR: Unknown field "{name}". Configuration seems to be not a valid iWrap file!'
+                raise ValueError(error_msg)
+            attr = getattr( self, name )
+            if isinstance( attr, Dictionarizable ):
+                attr.from_dict( value )
+            else:
+                setattr( self, name, value )
     @abstractmethod
     def to_dict(self):
         dict_ = vars( self )
         for key, value in dict_.items():
             if isinstance( value, Dictionarizable ):
                 dict_[key] = value.to_dict()
-            if isinstance( value, List ):
+            elif isinstance( value, List ):
                 dict_[key] = [item.to_dict() if isinstance( item, Dictionarizable )  else item for item in value ]
 
         return dict_
