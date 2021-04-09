290bfaaf1d43e84e0b82f5966a93c379432a4558
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-04-09 13:31:02 +0200

    Add label above FileBrowser widget object

--- iwrap/gui/settings/code_parameters_pane.py
+++ iwrap/gui/settings/code_parameters_pane.py
@@ -11,10 +11,10 @@ class CodeParametersPane( ttk.Frame, IWrapPane ):
         super().__init__( master )
 
         # XML file path browser dialog
-        self._xml_browser = FileBrowser(self, file_type='xml').pack()
+        self._xml_browser = FileBrowser(self, file_type='xml', label_text="Code parameters file:").pack()
         
         # XSD file path browser dialog
-        self._xsd_browser = FileBrowser(self, file_type='xsd').pack()
+        self._xsd_browser = FileBrowser(self, file_type='xsd', label_text="Schema file:").pack()
 
         # Any file path browser dialog
         self._any_browser = FileBrowser(self).pack()
@@ -28,16 +28,21 @@ class CodeParametersPane( ttk.Frame, IWrapPane ):
 
 
 class FileBrowser(ttk.Frame):
-    def __init__(self, master=None, file_type=None) -> None:
+    def __init__(self, master=None, file_type=None, label_text="") -> None:
         super().__init__(master)
         self.file_type, self.file_type_title = self.__define_file_type(file_type)
         
+        # A label above widget
+        self.label = ttk.Label(self, text=label_text)
+        self.label.pack(side=tk.TOP, anchor=tk.SW, expand=True)
+
         # A button to browse files
         self.button = ttk.Button(self, text = f"Browse {self.file_type_title} File", command = self.action_open)
-        self.button.pack(side='right', expand=False, fill=tk.X, padx=5)
-
+        self.button.pack(side=tk.RIGHT, expand=True, fill=tk.X, padx=5)
+        
+        # An entry to display path dialog
         self.path_dialog = ttk.Entry(self, state='readonly')
-        self.path_dialog.pack(side='left', fill=tk.X, expand=True)
+        self.path_dialog.pack(side=tk.LEFT, expand=True, fill=tk.X, padx=5)
 
         self.pack(expand=False, fill=tk.X, pady=5, ipady=5, padx=5, ipadx=5)
     
