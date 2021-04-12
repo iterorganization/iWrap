36d420bed7885b0d98180d8139f790e04ca57085
nkraszewska <natkrasz5@gmail.com>
2021-04-12 10:32:05 +0200

    Add Notebook with tabs, Add combobox frame and tab frame

--- iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
+++ iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
@@ -15,14 +15,32 @@ class FortranPane( ttk.Frame, IWrapPane ):
 
         # LABEL FRAME
         labelframe = ttk.LabelFrame(self, text="Language specific settings", borderwidth=2, relief="groove")
-        labelframe.pack(fill=tk.BOTH, expand=1, pady=15)
-        labelframe.grid_columnconfigure(1, weight=1)
+        labelframe.pack(fill=tk.BOTH, expand=1)
+
+        # COMBOBOX FRAME
+        combobox_frame = ttk.Frame(labelframe)
+        combobox_frame.pack(fill=tk.BOTH, side=tk.TOP, expand=0, anchor=tk.NW)
+        combobox_frame.grid_columnconfigure(1, weight=1)
 
         # COMBOBOX
-        ttk.Label(labelframe, text="Compiler:").grid(column=0, row=0, padx=10, pady=5, sticky=(tk.W, tk.N))
-        self.compiler_combobox = ttk.Combobox(labelframe, state='readonly')
+        ttk.Label(combobox_frame, text="Compiler:").grid(column=0, row=0, padx=10, pady=5, sticky=(tk.W, tk.N))
+        self.compiler_combobox = ttk.Combobox(combobox_frame, state='readonly')
         self.compiler_combobox['values'] = self.combobox_values
         self.compiler_combobox.grid(column=1, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
+
+        # TABS FRAME
+        tab_frame = ttk.Frame(labelframe)
+        tab_frame.pack(fill=tk.BOTH, expand=1, anchor=tk.NW)
+
+        # NOTEBOOK WITH TABS
+        self.tab_control = ttk.Notebook(tab_frame)
+        self.feature_tab = ttk.Frame(self.tab_control)
+        self.sys_lib_tab = ttk.Frame(self.tab_control)
+        self.cus_lib_tab = ttk.Frame(self.tab_control)
+        self.tab_control.add(self.feature_tab, text="Features")
+        self.tab_control.add(self.sys_lib_tab, text="System libraries")
+        self.tab_control.add(self.cus_lib_tab, text="Custom libraries")
+        self.tab_control.pack(fill=tk.BOTH, expand=1, anchor=tk.NW)
 
     def update_settings(self):
         pass
