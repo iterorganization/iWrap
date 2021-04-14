d010a988e470e6ccc350c2f1df7f8608afce2028
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-04-14 09:27:01 +0200

    Add prototype class TextBox

--- iwrap/gui/settings/signature_pane.py
+++ iwrap/gui/settings/signature_pane.py
@@ -7,8 +7,6 @@ from iwrap.gui.generics import IWrapPane
 class SignaturePane( ttk.Frame, IWrapPane ):
     def __init__(self, master=None):
         super().__init__( master )
-        ttk.Label( self, text="Place for SIGNATURE", borderwidth=1, relief="solid" ).pack( fill=tk.BOTH, expand=1 )
-
 
     def update_settings(self):
         pass
@@ -16,3 +14,7 @@ class SignaturePane( ttk.Frame, IWrapPane ):
     def reload(self):
         pass
 
+
+class TextBox(ttk.Frame):
+    def __init__(self, master=None) -> None:
+        super().__init__(master)
