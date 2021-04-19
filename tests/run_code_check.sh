efff5339e09745ff901580ef4ad4cbc7c245f6cf
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-04-19 12:40:07 +0200

    Updated code check script.

--- tests/run_code_check.sh
+++ tests/run_code_check.sh
@@ -1,2 +1,9 @@
-echo "~~~~~====================PYLINT CODE CHECK====================~~~~~"
-python -m pylint -E --output-format=pylint_junit.JUnitReporter iwrap > pylint.xml
+#!/bin/sh
+# Task: Setup Environment and Run Pylint code check. Publish report artifacts with python modules and imas env modules.
+set -e
+my_dir=$(dirname $0)
+envs_dir=$(dirname $0)/../envs/iter-bamboo
+
+. $envs_dir/00_load_imas_env.sh
+. $envs_dir/10_python_set_env.sh
+. $envs_dir/03_report_module_list.sh
