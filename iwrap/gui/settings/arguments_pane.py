b8611caf04a177de7d75a09bf46f247f6f4a024d
Natalia Kraszewska <natkrasz5@gmail.com>
2021-04-20 09:54:11 +0200

    Add combobox frame

--- iwrap/gui/settings/arguments_pane.py
+++ iwrap/gui/settings/arguments_pane.py
@@ -9,14 +9,21 @@ class ArgumentsPane( ttk.Frame, IWrapPane ):
         super().__init__( master )
 
         # LABEL FRAME
-        labelframe = ttk.LabelFrame(self, text="Arguments", borderwidth=2, relief="groove")
-        labelframe.pack(fill=tk.BOTH, pady=10, expand=1)
-        labelframe.grid_columnconfigure(1, weight=1)
+        labelframe = ttk.LabelFrame(self, text="Arguments", borderwidth=2, relief="groove", height=200)
+        labelframe.pack(fill=tk.X, pady=10)
+        labelframe.grid_columnconfigure(0, weight=1)
+        labelframe.grid_rowconfigure(1, weight=1)
+
+        # COMBOBOX FRAME
+        combobox_frame = tk.Frame(labelframe, pady=5)
+        combobox_frame.grid(row=0, sticky="ew")
+        combobox_frame.grid_rowconfigure(0, weight=1)
+        combobox_frame.grid_columnconfigure(1, weight=1)
 
         # COMBOBOX
         self.combobox_values = ['IDS', 'HDC']
-        ttk.Label(labelframe, text="Data type:").grid(column=0, row=0, padx=10, pady=5, sticky=(tk.W, tk.N))
-        self.data_type_combobox = ttk.Combobox(labelframe, state='readonly')
+        ttk.Label(combobox_frame, text="Data type:").grid(column=0, row=0, padx=10, pady=5, sticky=(tk.W, tk.N))
+        self.data_type_combobox = ttk.Combobox(combobox_frame, state='readonly')
         self.data_type_combobox['values'] = self.combobox_values
         self.data_type_combobox.current(0)
         self.data_type_combobox.grid(column=1, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
