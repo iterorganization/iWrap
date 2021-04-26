ae20e929996222a2ab986ef6a62cfb8846fe87e9
Natalia Kraszewska <natkrasz5@gmail.com>
2021-04-26 15:37:24 +0200

    Add radiobuttons to table widget

--- iwrap/gui/widgets/table.py
+++ iwrap/gui/widgets/table.py
@@ -15,47 +15,50 @@ class Table( ttk.Frame ):
         for row_number, row in enumerate(data):
             table_row = Row(row_number + 1, row, master)
             self.rows.append(table_row)
-            for entry in table_row.row_entries:
-                entry.entry.bind("<1>", lambda event, parent_row=table_row: self.select_row(parent_row))
+            for row_cell in table_row.row_cells:
+                row_cell.cell.bind("<1>", lambda event, parent_row=table_row: self.select_row(parent_row))
 
     def select_row(self, parent_row):
         self.selected_row = parent_row.row_number
         for row in self.rows:
-            for entry in row.row_entries:
-                if entry.row_number == self.selected_row:
-                    entry.entry.config(readonlybackground="lightgray")
+            for row_cell in row.row_cells:
+                if row_cell.row_number == self.selected_row:
+                    row_cell.change_color_to_lightgray()
                 else:
-                    entry.entry.config(readonlybackground="white")
+                    row_cell.change_color_to_white()
 
     def delete_row(self):
         for row in self.rows:
             if row.row_number == self.selected_row:
-                for entry in row.row_entries:
-                    entry.entry.destroy()
-                    del entry
+                for row_cell in row.row_cells:
+                    row_cell.cell.destroy()
+                    del row_cell
                 self.rows.remove(row)
                 del row
+        # print("---delete---")
+        # for row in self.rows:
+        #     print(f"{row.row_number} -> {row.row_cells[0].value}")
         self.update_rows()
 
     def update_rows(self):
         for row_number, row in enumerate(self.rows):
-            for entry in row.row_entries:
-                entry.row_number = row_number + 1
+            for row_cell in row.row_cells:
+                row_cell.row_number = row_number + 1
             row.row_number = row_number + 1
         self.selected_row = None
 
     @staticmethod
     def move_row_up(row_to_move):
-        for entry in row_to_move.row_entries:
-            entry.row_number = row_to_move.row_number - 1
-            entry.entry.grid(row=row_to_move.row_number - 1, column=entry.column_number, sticky="ew")
+        for row_cell in row_to_move.row_cells:
+            row_cell.row_number = row_to_move.row_number - 1
+            row_cell.cell.grid(row=row_to_move.row_number - 1, column=row_cell.column_number, sticky="ew")
         row_to_move.row_number = row_to_move.row_number - 1
 
     @staticmethod
     def move_row_down(row_to_move):
-        for entry in row_to_move.row_entries:
-            entry.row_number = row_to_move.row_number + 1
-            entry.entry.grid(row=row_to_move.row_number + 1, column=entry.column_number, sticky="ew")
+        for row_cell in row_to_move.row_cells:
+            row_cell.row_number = row_to_move.row_number + 1
+            row_cell.cell.grid(row=row_to_move.row_number + 1, column=row_cell.column_number, sticky="ew")
         row_to_move.row_number = row_to_move.row_number + 1
 
     def row_up_feature(self):
@@ -85,20 +88,55 @@ class Table( ttk.Frame ):
 class Row( ttk.Frame ):
     def __init__(self, row, data, master=None):
         self.row_number = row
-        self.row_entries = []
+        self.row_cells = []
+        self.v = tk.BooleanVar()
+        self.v.set(True)
         for idx, elem in enumerate(data):
-            self.row_entries.append(RowEntry(row, idx, elem, master))
+            if isinstance(elem, str):
+                self.row_cells.append(RowEntry(row, idx, elem, master))
+            if isinstance(elem, bool):
+                self.row_cells.append(RowRadioButton(row, idx, elem, master))
+
+        self.make_radio_buttons_pairs()
+
+    def make_radio_buttons_pairs(self):
+        self.v = tk.BooleanVar()
+        self.v.set(True)
+        for row_cell in self.row_cells:
+            if isinstance(row_cell, RowRadioButton):
+                row_cell.cell.config(variable=self.v, value=row_cell.value)
+
+
+class RowRadioButton(ttk.Frame):
+    def __init__(self, row, column, value, master=None):
+        self.row_number = row
+        self.column_number = column
+        self.value = value
+        self.cell = tk.Radiobutton(master, bg="white")
+        self.cell.grid(row=row, column=column, sticky="ew")
+
+    def change_color_to_lightgray(self):
+        self.cell.config(bg="lightgray")
+
+    def change_color_to_white(self):
+        self.cell.config(bg="white")
 
 
 class RowEntry( ttk.Frame ):
     def __init__(self, row, column, text, master=None):
         self.row_number = row
         self.column_number = column
-        self.text = text
+        self.value = text
         row_text = tk.StringVar()
         row_text.set(text)
-        self.entry = tk.Entry(master, textvariable=row_text, state='readonly', readonlybackground="white", width=6)
-        self.entry.grid(row=row, column=column, sticky="ew")
+        self.cell = tk.Entry(master, textvariable=row_text, state='readonly', readonlybackground="white", width=6)
+        self.cell.grid(row=row, column=column, sticky="ew")
+
+    def change_color_to_lightgray(self):
+        self.cell.config(readonlybackground="lightgray")
+
+    def change_color_to_white(self):
+        self.cell.config(readonlybackground="white")
 
 
 class Column( ttk.Frame ):
