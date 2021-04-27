05237d241d7e41942d087cc229e619a74cd3f3f2
Natalia Kraszewska <natkrasz5@gmail.com>
2021-04-27 09:58:52 +0200

    Add scrollable frame, change some methods to private

--- iwrap/gui/widgets/table.py
+++ iwrap/gui/widgets/table.py
@@ -4,34 +4,27 @@ from tkinter import ttk
 
 class Table( ttk.Frame ):
     def __init__(self, data, columns, master=None):
-
-        # CANVAS, FRAME AND SCROLLBAR
-        self.canvas = tk.Canvas(master, borderwidth=0)
-        self.frame = tk.Frame(self.canvas)
-        vsb = tk.Scrollbar(master, orient=tk.VERTICAL, command=self.canvas.yview)
-        self.canvas.configure(yscrollcommand=vsb.set)
-        vsb.pack(side=tk.RIGHT, fill=tk.Y)
-        self.canvas.pack(fill=tk.BOTH, expand=1)
-        self.canvas.create_window((4, 4), window=self.frame, anchor="nw")
-        self.frame.bind("<Configure>", self.on_frame_configure)
-
+        super().__init__(master)
+        self.frame = ScrollableFrame(master)
+        self.data = data
+        self.columns = columns
         self.selected_row = None
-        # ADD COLUMNS
-        for idx, column in enumerate(columns):
+        self._add_data()
+        self.frame.update()
+
+    def _add_data(self):
+        for idx, column in enumerate(self.columns):
+            self.frame.columnconfigure(idx, weight=1)
             Column(idx, column, self.frame)
 
-        # ADD ROWS
         self.rows = []
-        for row_number, row in enumerate(data):
+        for row_number, row in enumerate(self.data):
             table_row = Row(row_number + 1, row, self.frame)
             self.rows.append(table_row)
             for row_cell in table_row.row_cells:
-                row_cell.cell.bind("<1>", lambda event, parent_row=table_row: self.select_row(parent_row))
+                row_cell.cell.bind("<1>", lambda event, parent_row=table_row: self._select_row(parent_row))
 
-    def on_frame_configure(self, event):
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-
-    def select_row(self, parent_row):
+    def _select_row(self, parent_row):
         self.selected_row = parent_row.row_number
         for row in self.rows:
             for row_cell in row.row_cells:
@@ -48,12 +41,9 @@ class Table( ttk.Frame ):
                     del row_cell
                 self.rows.remove(row)
                 del row
-        # print("---delete---")
-        # for row in self.rows:
-        #     print(f"{row.row_number} -> {row.row_cells[0].value}")
-        self.update_rows()
+        self._update_rows()
 
-    def update_rows(self):
+    def _update_rows(self):
         for row_number, row in enumerate(self.rows):
             for row_cell in row.row_cells:
                 row_cell.row_number = row_number + 1
@@ -61,14 +51,14 @@ class Table( ttk.Frame ):
         self.selected_row = None
 
     @staticmethod
-    def move_row_up(row_to_move):
+    def _move_row_up(row_to_move):
         for row_cell in row_to_move.row_cells:
             row_cell.row_number = row_to_move.row_number - 1
             row_cell.cell.grid(row=row_to_move.row_number - 1, column=row_cell.column_number, sticky="ew")
         row_to_move.row_number = row_to_move.row_number - 1
 
     @staticmethod
-    def move_row_down(row_to_move):
+    def _move_row_down(row_to_move):
         for row_cell in row_to_move.row_cells:
             row_cell.row_number = row_to_move.row_number + 1
             row_cell.cell.grid(row=row_to_move.row_number + 1, column=row_cell.column_number, sticky="ew")
@@ -79,9 +69,9 @@ class Table( ttk.Frame ):
             top_row = [row for row in self.rows if row.row_number == self.selected_row - 1][0]
             current_row = [row for row in self.rows if row.row_number == self.selected_row][0]
 
-            self.move_row_up(current_row)
+            self._move_row_up(current_row)
             self.selected_row = self.selected_row - 1
-            self.move_row_down(top_row)
+            self._move_row_down(top_row)
             self.rows.sort(key=lambda x: x.row_number, reverse=False)
 
     def row_down_feature(self):
@@ -89,16 +79,16 @@ class Table( ttk.Frame ):
             bottom_row = [row for row in self.rows if row.row_number == self.selected_row + 1][0]
             current_row = [row for row in self.rows if row.row_number == self.selected_row][0]
 
-            self.move_row_down(current_row)
+            self._move_row_down(current_row)
             self.selected_row = self.selected_row + 1
-            self.move_row_up(bottom_row)
+            self._move_row_up(bottom_row)
             self.rows.sort(key=lambda x: x.row_number, reverse=False)
 
     def add_row(self):
         pass
 
 
-class Row( ttk.Frame ):
+class Row:
     def __init__(self, row, data, master=None):
         self.row_number = row
         self.row_cells = []
@@ -110,9 +100,9 @@ class Row( ttk.Frame ):
             if isinstance(elem, bool):
                 self.row_cells.append(RowRadioButton(row, idx, elem, master))
 
-        self.make_radio_buttons_pairs()
+        self._make_radio_buttons_pairs()
 
-    def make_radio_buttons_pairs(self):
+    def _make_radio_buttons_pairs(self):
         self.v = tk.BooleanVar()
         self.v.set(True)
         for row_cell in self.row_cells:
@@ -120,7 +110,7 @@ class Row( ttk.Frame ):
                 row_cell.cell.config(variable=self.v, value=row_cell.value)
 
 
-class RowRadioButton(ttk.Frame):
+class RowRadioButton:
     def __init__(self, row, column, value, master=None):
         self.row_number = row
         self.column_number = column
@@ -135,7 +125,7 @@ class RowRadioButton(ttk.Frame):
         self.cell.config(bg="white")
 
 
-class RowEntry( ttk.Frame ):
+class RowEntry:
     def __init__(self, row, column, text, master=None):
         self.row_number = row
         self.column_number = column
@@ -152,9 +142,30 @@ class RowEntry( ttk.Frame ):
         self.cell.config(readonlybackground="white")
 
 
-class Column( ttk.Frame ):
+class Column:
     def __init__(self, column, text, master=None):
         type_text = tk.StringVar()
         type_text.set(text)
         tk.Entry(master, textvariable=type_text, state='readonly', width=14, justify='center')\
             .grid(row=0, column=column, sticky="ew")
+
+
+class ScrollableFrame( ttk.Frame ):
+    def __init__(self, master):
+        scrollbar = tk.Scrollbar(master)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y, expand=False)
+
+        self.canvas = tk.Canvas(master, yscrollcommand=scrollbar.set)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        scrollbar.config(command=self.canvas.yview)
+        self.canvas.bind('<Configure>', self._fill_canvas)
+
+        ttk.Frame.__init__(self, master)
+        self.windows_item = self.canvas.create_window(0, 0, window=self, anchor=tk.NW)
+
+    def _fill_canvas(self, event):
+        self.canvas.itemconfig(self.windows_item, width=event.width)
+
+    def update(self):
+        self.update_idletasks()
+        self.canvas.config(scrollregion=self.canvas.bbox(self.windows_item))
