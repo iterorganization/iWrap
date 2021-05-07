15ef1f0858f57d46cec98013855a3ec176efe0dd
Bartek Palak <bartek.palak@man.poznan.pl>
2021-05-07 10:32:25 +0200

    Handling generation engine startup and basic functionality

--- iwrap/generation_engine/engine.py
+++ iwrap/generation_engine/engine.py
@@ -1,12 +1,60 @@
+from typing import Set, List
+
+from iwrap.generation_engine.base_classes import ActorGenerator
+from iwrap.generation_engine.generators_mgmt import GeneratorRegistry
 from iwrap.settings.platform.platform_settings import PlatformSettings
 
 
 def startup():
-    PlatformSettings().initialize()
 
-    # TODO: initialize generators
     pass
 
 
 class Engine:
-    pass
+    __class_instance = None
+    _registry: GeneratorRegistry = GeneratorRegistry()
+    _active_generator: ActorGenerator = None
+
+    def __new__(cls):
+        if cls.__class_instance is None:
+            cls.__class_instance = object.__new__(cls)
+        return cls.__class_instance
+
+    @classmethod
+    def get_code_signature(cls) -> str:
+        return cls._active_generator.get_code_signature()
+
+    @property
+    def active_generator(self):
+        return Engine._active_generator
+
+    @active_generator.setter
+    def active_generator(self, value):
+        if Engine._active_generator == value:
+            return
+
+        if isinstance(value, str):
+            value = Engine._registry.get_generator(value)
+
+        Engine._active_generator = value
+        Engine._active_generator.init()
+
+    @property
+    def registered_generators(self) -> List[ActorGenerator]:
+        return Engine._registry.registered_generators
+
+    def startup(self):
+        PlatformSettings().initialize()
+
+        # Initialization of registry of generators
+        generators_registry = GeneratorRegistry()
+        generators_registry.initialize()
+
+        # set default generator
+        registered_generators = generators_registry.registered_generators
+        if len(registered_generators) > 0:
+            Engine._active_generator = registered_generators[0]
+            Engine._active_generator.init()
+
+    def generate_actor(self):
+        Engine._active_generator.generate()
