d51af6431ab390a970dc5a7c2bceb004063eb934
Natalia Kraszewska <natkrasz5@gmail.com>
2021-05-11 09:31:24 +0200

    Add docstrings to ScrollableFrame class

--- iwrap/gui/widgets/scrollable_frame.py
+++ iwrap/gui/widgets/scrollable_frame.py
@@ -3,7 +3,27 @@ from tkinter import ttk
 
 
 class ScrollableFrame( ttk.Frame ):
+    """Scrollable frame widget.
+
+    Attributes:
+        scrollbar(Scrollbar): Slide controller that is used to implement vertically scrolled Canvas.
+        canvas(Canvas): Area intended for placing scrollbar and frame.
+
+    ``Example``:
+
+    .. code-block:: python
+
+        your_scrollable_frame = ScrollableFrame(root)
+        Label(your_scrollable_frame, text="Hello world!").pack()
+        your_scrollable_frame = your_scrollable_frame.update()
+
+    """
     def __init__(self, master):
+        """ Initialize the scrollable frame.
+
+        Args:
+            master: Parent widget from Tkinter class. Default to None.
+        """
         scrollbar = tk.Scrollbar(master)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y, expand=0)
 
@@ -16,8 +36,18 @@ class ScrollableFrame( ttk.Frame ):
         self.windows_item = self.canvas.create_window(0, 0, window=self, anchor=tk.NW)
 
     def _fill_canvas(self, event):
+        """The method is called always when configuring resources of a widget are changed.
+
+        Args:
+            event(Event): Configure resources of canvas event.
+        """
         self.canvas.itemconfig(self.windows_item, width=event.width)
 
     def update(self):
+        """Update canvas scrollable region when something new is added to the scrollable frame.
+
+        Note:
+            The update() should be called when something new is added to the scrollable frame.
+        """
         self.update_idletasks()
         self.canvas.config(scrollregion=self.canvas.bbox(self.windows_item))
