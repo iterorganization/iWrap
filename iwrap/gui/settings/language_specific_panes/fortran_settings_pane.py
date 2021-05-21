7da3e939b11ca722b4a933395f8b668432efbe76
Natalia Kraszewska <natkrasz5@gmail.com>
2021-05-21 10:33:52 +0200

    Add docstrings, clean code

--- iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
+++ iwrap/gui/settings/language_specific_panes/fortran_settings_pane.py
@@ -10,11 +10,26 @@ from iwrap.settings.project import ProjectSettings
 
 
 class FortranPane( ttk.Frame, IWrapPane ):
-    def __init__(self, master=None):
-        super().__init__( master )
+    """The FortranPane contains a combobox for selecting compiler and three tabs including feature frame, system library
+    frame, and custom library frame. Feature frame contains two Combobox widgets and enables the selection of MPI
+    Flavour and OpenMPI, system library frame contains the Table widget with system libraries and the custom library
+    frame contains the ListBox widget with custom libraries.
 
+    Attributes:
+        settings (FortranSpecificSettings): The project settings for fortran language pane.
+        compiler_combobox (ttk.Combobox): The compiler combobox.
+        feature_pane (FeaturesPane): The FeaturesPane object.
+        system_libraries_pane (SystemLibrariesPane): The SystemLibrariesPane object.
+        custom_libraries_pane (CustomLibrariesPane): The CustomLibrariesPane object.
+    """
+    def __init__(self, master=None):
+        """Initialize the FortranPane class object.
+
+        Args:
+            master (ttk.Frame): The master frame.
+        """
+        super().__init__( master )
         self.settings = FortranSpecificSettings()
-        self.combobox_values = ['Intel Fortran (ifort)', 'GNU Compiler Collection (fortran)', 'Intel']
 
         # LABEL FRAME
         labelframe = ttk.LabelFrame(self, text="Language specific settings", borderwidth=2, relief="groove")
@@ -28,7 +43,7 @@ class FortranPane( ttk.Frame, IWrapPane ):
         # COMBOBOX
         ttk.Label(combobox_frame, text="Compiler:").grid(column=0, row=0, padx=10, pady=5, sticky=(tk.W, tk.N))
         self.compiler_combobox = ttk.Combobox(combobox_frame, state='readonly')
-        self.compiler_combobox['values'] = self.combobox_values
+        self.compiler_combobox['values'] = ['Intel Fortran (ifort)', 'GNU Compiler Collection (fortran)', 'Intel']
         self.compiler_combobox.current(0)
         self.compiler_combobox.grid(column=1, row=0, padx=10, pady=5, sticky=(tk.W, tk.E))
 
@@ -38,19 +53,22 @@ class FortranPane( ttk.Frame, IWrapPane ):
 
         # NOTEBOOK WITH TABS
         tab_control = ttk.Notebook(tab_frame)
-        self.sys_lib_tab = ttk.Frame(tab_control)
-        self.cus_lib_tab = ttk.Frame(tab_control)
-        self.feature_lib_tab = ttk.Frame(tab_control)
-        tab_control.add(self.feature_lib_tab, text="Features")
-        tab_control.add(self.sys_lib_tab, text="System libraries")
-        tab_control.add(self.cus_lib_tab, text="Custom libraries")
+        sys_lib_tab = ttk.Frame(tab_control)
+        cus_lib_tab = ttk.Frame(tab_control)
+        feature_lib_tab = ttk.Frame(tab_control)
+        tab_control.add(feature_lib_tab, text="Features")
+        tab_control.add(sys_lib_tab, text="System libraries")
+        tab_control.add(cus_lib_tab, text="Custom libraries")
         tab_control.pack(fill=tk.BOTH, expand=1, anchor=tk.NW, pady=5)
 
-        self.feature_pane = FeaturesPane(self)
-        self.system_libraries_pane = SystemLibrariesPane(self)
-        self.custom_libraries_pane = CustomLibrariesPane(self)
+        self.feature_pane = FeaturesPane(feature_lib_tab)
+        self.system_libraries_pane = SystemLibrariesPane(sys_lib_tab)
+        self.custom_libraries_pane = CustomLibrariesPane(cus_lib_tab)
 
     def reload(self):
+        """Reload system settings from the ProjectSettings, set compiler to the Combobox widget as current value.
+        Call SystemLibrariesPane, CustomLibrariesPane, and FeaturesPane reload methods.
+        """
         dict_settings = ProjectSettings.get_settings().code_description.language_specific
         if dict_settings is None:
             self.settings.clear()
@@ -64,24 +82,39 @@ class FortranPane( ttk.Frame, IWrapPane ):
         self.custom_libraries_pane.reload()
 
     def update_settings(self):
+        """Update compiler value in the ProjectSettings. Call SystemLibrariesPane, CustomLibrariesPane,
+        and FeaturesPane update_settings methods.
+        """
         compiler = self.compiler_combobox.get()
         ProjectSettings.get_settings().code_description.language_specific['compiler'] = compiler
-        self.feature_pane.update_settings()
         self.system_libraries_pane.update_settings()
         self.custom_libraries_pane.update_settings()
+        self.feature_pane.update_settings()
 
 
 class SystemLibrariesPane:
-    def __init__(self, master=None):
-        self.settings = master.settings
-        master_frame = master.sys_lib_tab
+    """The SystemLibrariesPane contains the Table widget with system libraries. Add button enables to add system library
+    to the Table, remove button enables to delete selected library from the Table.
 
-        # TREE VIEW FRAME
-        self.tree_view_frame = ttk.Frame(master_frame)
-        self.tree_view_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=1, anchor=tk.NW)
+    Attributes:
+        settings (FortranSpecificSettings): The project settings for fortran language pane.
+        columns (list[Column]): The list of the Column class objects.
+        table (Table): The table widget.
+    """
+    def __init__(self, master=None):
+        """Initialize the SystemLibrariesPane class object.
+
+        Args:
+            master (ttk.Frame): The master frame.
+        """
+        self.settings = FortranSpecificSettings()
+
+        # TABLE FRAME
+        table_frame = ttk.Frame(master)
+        table_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=1, anchor=tk.NW)
 
         # BUTTONS FRAMES
-        buttons_frame = ttk.Frame(master_frame, width=100)
+        buttons_frame = ttk.Frame(master, width=100)
         buttons_frame.pack(fill=tk.BOTH, side=tk.RIGHT, expand=0, anchor=tk.NE)
         buttons_center_frame = ttk.Frame(buttons_frame)
         buttons_center_frame.place(in_=buttons_frame, anchor="center", relx=.5, rely=.5)
@@ -94,22 +127,27 @@ class SystemLibrariesPane:
         # BUTTONS
         add_button = ttk.Button(buttons_center_frame, text="Add...", width=10)
         add_button.pack(side=tk.TOP, expand=1, pady=5)
-        # ttk.Button(buttons_center_frame, text="Info", width=10).pack(side=tk.TOP, expand=1, pady=5)
         remove_button = ttk.Button(buttons_center_frame, text="Remove", width=10)
         remove_button.pack(side=tk.TOP, expand=1, pady=5)
 
-        self.table = Table([], self.columns, self.tree_view_frame, [remove_button])
+        self.table = Table([], self.columns, table_frame, [remove_button])
         add_button['command'] = lambda: self.table.add_row("system library")
         remove_button['command'] = self.table.delete_row
 
-    def add_table_data(self):
+    def __add_table_data(self):
+        """Add system libraries to the table.
+        """
         data = []
         for sys_lib in self.settings.system_libraries:
             data.append([sys_lib, 'example_info', 'example_description'])
 
         self.table.add_new_table(data, self.columns)
 
-    def get_data_from_table(self):
+    def __get_data_from_table(self):
+        """Get system libraries names from the table.
+
+        Returns (list): The list with system libraries names from the table.
+        """
         system_libraries = self.table.get_data_from_table()
         libraries_name = []
         for system_library in system_libraries:
@@ -118,38 +156,53 @@ class SystemLibrariesPane:
         return libraries_name
 
     def reload(self):
+        """Reload system settings from the ProjectSettings and add system libraries to the Table widget.
+        """
         dict_settings = ProjectSettings.get_settings().code_description.language_specific
         if dict_settings is None:
             self.settings.clear()
         else:
             self.settings.from_dict(dict_settings)
 
-        self.add_table_data()
+        self.__add_table_data()
 
     def update_settings(self):
-        libraries_name = self.get_data_from_table()
+        """Update system libraries in the ProjectSettings.
+        """
+        libraries_name = self.__get_data_from_table()
         ProjectSettings.get_settings().code_description.language_specific['system_libraries'] = libraries_name
 
 
 class CustomLibrariesPane:
+    """The CustomLibrariesPane contains the ListBox widget with the custom libraries. Add button enables to add library
+    path from filedialog to the ListBox, remove button enables to delete selected library from the ListBox.
+
+    Attributes:
+        settings (FortranSpecificSettings): The project settings for fortran language pane.
+        listbox (tk.Listbox): The listbox contains custom libraries.
+    """
     def __init__(self, master=None):
-        self.settings = master.settings
-        master_frame = master.cus_lib_tab
+        """Initialize the CustomLibrariesPane class object.
+
+        Args:
+            master (ttk.Frame): The master frame.
+        """
+        self.settings = FortranSpecificSettings()
 
         # LIBRARY PATH FRAME
-        library_path_frame = tk.Frame(master_frame)
+        library_path_frame = tk.Frame(master)
         library_path_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=1, anchor=tk.NW)
 
         # BUTTONS FRAMES
-        buttons_frame = ttk.Frame(master_frame, width=100)
+        buttons_frame = ttk.Frame(master, width=100)
         buttons_frame.pack(fill=tk.BOTH, side=tk.RIGHT, expand=0, anchor=tk.NE)
         buttons_center_frame = ttk.Frame(buttons_frame)
         buttons_center_frame.place(in_=buttons_frame, anchor="center", relx=.5, rely=.5)
 
         # BUTTONS
-        ttk.Button(buttons_center_frame, text="Add...", command=self.add_on_click, width=10)\
+        ttk.Button(buttons_center_frame, text="Add...", command=self.__add_on_click, width=10)\
             .pack(side=tk.TOP, expand=1, pady=5)
-        ttk.Button(buttons_center_frame, text="Remove", command=self.remove_on_click, width=10)\
+        ttk.Button(buttons_center_frame, text="Remove", command=self.__remove_on_click, width=10)\
             .pack(side=tk.TOP, expand=1, pady=5)
 
         # LIBRARY PATH LABEL
@@ -162,32 +215,42 @@ class CustomLibrariesPane:
         self.listbox = tk.Listbox(library_path_frame, yscrollcommand=scrollbar.set, selectmode=tk.MULTIPLE)
         self.listbox.pack(side=tk.TOP, fill=tk.BOTH, anchor='nw', expand=1)
 
-    def add_custom_lib_from_settings(self):
+    def __add_custom_lib_from_settings(self):
+        """Add custom libraries from the ProjectSettings to the ListBox widget.
+        """
         self.listbox.delete(0, tk.END)
         for cus_lib in self.settings.custom_libraries:
             self.listbox.insert(tk.END, cus_lib)
 
-    def add_on_click(self):
+    def __add_on_click(self):
+        """Open the filedialog and add selected path to the ListBox widget.
+        """
         path = tk.filedialog.askopenfilename()
         self.listbox.insert(tk.END, path)
         self.settings.custom_libraries.append(path)
 
-    def remove_on_click(self):
+    def __remove_on_click(self):
+        """Remove clicked custom library from the ListBox widget.
+        """
         selected_paths = self.listbox.curselection()
         for index in selected_paths[::-1]:
             self.listbox.delete(index)
             self.settings.custom_libraries.pop(index)
 
     def reload(self):
+        """Reload custom_libraries list from the ProjectSettings and add it to the ListBox widget.
+        """
         dict_settings = ProjectSettings.get_settings().code_description.language_specific
         if dict_settings is None:
             self.settings.clear()
         else:
             self.settings.from_dict(dict_settings)
 
-        self.add_custom_lib_from_settings()
+        self.__add_custom_lib_from_settings()
 
     def update_settings(self):
+        """Update custom_libraries in the ProjectSettings.
+        """
         custom_libraries = list(self.listbox.get(0, tk.END))
         ProjectSettings.get_settings().code_description.language_specific['custom_libraries'] = custom_libraries
 
@@ -202,16 +265,15 @@ class FeaturesPane:
     """
 
     def __init__(self, master=None):
-        """Initialize the FeaturesPane object.
+        """Initialize the FeaturesPane class object.
 
         Args:
-            master (FortranPane): The master frame.
+            master (ttk.Frame): The master frame.
         """
-        master_frame = master.feature_lib_tab
-        self.settings = master.settings
+        self.settings = FortranSpecificSettings()
 
         # LABEL FRAME
-        labelframe = ttk.LabelFrame(master_frame, text="Computation", borderwidth=2, relief="groove")
+        labelframe = ttk.LabelFrame(master, text="Computation", borderwidth=2, relief="groove")
         labelframe.pack(side=tk.LEFT, fill=tk.BOTH, expand=1, pady=5)
 
         # COMBOBOX MPI Flavour
