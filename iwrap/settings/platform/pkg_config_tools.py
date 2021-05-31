e72ec3a7ac7a9506f808c1647dee549b74139d65
Natalia Kraszewska <nkraszewska@man.poznan.pl>
2021-05-31 10:13:42 +0200

    Change initialize and to_dict methods to private, add system_lib_dict property, update docstrings

--- iwrap/settings/platform/pkg_config_tools.py
+++ iwrap/settings/platform/pkg_config_tools.py
@@ -8,6 +8,10 @@ class PkgConfigTools:
         pkg_config_list (list): The list contains pkg configs.
         PKG_CONFIG_CMD (str): The config command.
         PKG_CONFIG_OPT_LIST_ALL (str): The command option enables to list all configs.
+
+    Properties:
+        system_lib_dict (dict): The dictionary with all pkg configs. Keys are pkg configs names, dictionary values are
+        info and descriptions.
     """
     PKG_CONFIG_CMD = "pkg-config"
     PKG_CONFIG_OPT_LIST_ALL = "--list-all"
@@ -15,25 +19,35 @@ class PkgConfigTools:
     def __init__(self):
         """Initialize the PkgConfigTools class object.
         """
-        self.pkg_config_list = None
-        self.initialize()
+        self.__pkg_config_list = None
+        self.__initialize()
+        self.__system_lib_dict = self.__to_dict()
 
-    def initialize(self):
+    @property
+    def system_lib_dict(self):
+        """Get dictionary with pkg configs.
+
+        Returns (dict): The dictionary with all pkg configs. Keys are pkg configs names, dictionary values are
+        info and descriptions.
+        """
+        return self.__system_lib_dict
+
+    def __initialize(self):
         """Get list of pkg configs.
         """
         process = subprocess.Popen([PkgConfigTools.PKG_CONFIG_CMD,
                                     PkgConfigTools.PKG_CONFIG_OPT_LIST_ALL], stdout=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        self.pkg_config_list = stdout.decode('ascii').splitlines()
+        self.__pkg_config_list = stdout.decode('ascii').splitlines()
 
-    def to_dict(self):
+    def __to_dict(self):
         """Return dictionary with all pkg configs.
 
         Returns (dict): The dictionary with all pkg configs. Keys are pkg configs names, dictionary values are
         info and descriptions.
         """
         pkg_config_dict = {}
-        for pkg_config in self.pkg_config_list:
+        for pkg_config in self.__pkg_config_list:
             split_config = pkg_config.split(' ', 1)
             name = split_config[0]
             full_description = split_config[1].strip().split(' - ')
@@ -53,7 +67,6 @@ class PkgConfigTools:
         Returns (dict/None): The dictionary with info and description values or None if the system library
         is not available.
         """
-        pkg_config_dict = self.to_dict()
-        if system_library in pkg_config_dict.keys():
-            return pkg_config_dict[system_library]
+        if system_library in self.system_lib_dict.keys():
+            return self.system_lib_dict[system_library]
         return None
