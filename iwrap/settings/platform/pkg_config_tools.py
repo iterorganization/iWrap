f89e06fbf4e3009d2e82e29a12412e61f968b8f9
Natalia Kraszewska <nkraszewska@man.poznan.pl>
2021-05-31 10:39:11 +0200

    Remove initialize method from init, update docstrings

--- iwrap/settings/platform/pkg_config_tools.py
+++ iwrap/settings/platform/pkg_config_tools.py
@@ -5,7 +5,6 @@ class PkgConfigTools:
     """The class enables to get a dictionary with all pkg configs and get info and description values for the system library.
 
     Attributes:
-        pkg_config_list (list): The list contains pkg configs.
         PKG_CONFIG_CMD (str): The config command.
         PKG_CONFIG_OPT_LIST_ALL (str): The command option enables to list all configs.
 
@@ -20,7 +19,6 @@ class PkgConfigTools:
         """Initialize the PkgConfigTools class object.
         """
         self.__pkg_config_list = None
-        self.__initialize()
         self.__system_lib_dict = self.__to_dict()
 
     @property
@@ -33,19 +31,12 @@ class PkgConfigTools:
         return self.__system_lib_dict
 
     def __initialize(self):
-        """Get list of pkg configs.
-        """
         process = subprocess.Popen([PkgConfigTools.PKG_CONFIG_CMD,
                                     PkgConfigTools.PKG_CONFIG_OPT_LIST_ALL], stdout=subprocess.PIPE)
         stdout, stderr = process.communicate()
         self.__pkg_config_list = stdout.decode('ascii').splitlines()
 
     def __to_dict(self):
-        """Return dictionary with all pkg configs.
-
-        Returns (dict): The dictionary with all pkg configs. Keys are pkg configs names, dictionary values are
-        info and descriptions.
-        """
         pkg_config_dict = {}
         for pkg_config in self.__pkg_config_list:
             split_config = pkg_config.split(' ', 1)
