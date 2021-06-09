9e7f76010add9475efbad9a0b0019555c71a1bfc
Natalia Kraszewska <nkraszewska@man.poznan.pl>
2021-06-09 11:43:47 +0200

    Add horizontal scrollbar, change text_editor state to readonly, update window when text appended

--- iwrap/gui/widgets/progress_monitor_window.py
+++ iwrap/gui/widgets/progress_monitor_window.py
@@ -11,26 +11,37 @@ class ProgressMonitorWindow(tk.Toplevel, TextIOBase):
 
     def __initialize_window(self):
         self.transient(self.master)
-        self.minsize(400, 300)
-        self.geometry('400x100')
-        self.resizable(width=False, height=False)
+        self.minsize(800, 600)
+        self.geometry('800x600')
+        self.resizable(width=True, height=False)
         self.title("iWrap - actor generation progress monitor")
         tk.Label(self, text="Actor generation in progress").pack(pady=10)
 
         # Textbox with scrollbar
-        scrollbar = ttk.Scrollbar(self)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y, pady=(5, 2), padx=2)
-        self.__text_editor = tk.Text(self)
+        yscrollbar = ttk.Scrollbar(self)
+        yscrollbar.pack(side=tk.RIGHT, fill=tk.Y, pady=(5, 2), padx=2)
+        xscrollbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL)
+        xscrollbar.pack(side=tk.BOTTOM, fill=tk.X, pady=(5, 2), padx=2)
+        self.__text_editor = tk.Text(self, wrap=tk.NONE)
         self.__text_editor.pack(side=tk.TOP, expand=True, fill=tk.BOTH, pady=(5, 2), padx=5)
-        scrollbar.config(command=self.__text_editor.yview)
-        self.__text_editor['yscrollcommand'] = scrollbar.set
+        yscrollbar.config(command=self.__text_editor.yview)
+        xscrollbar.config(command=self.__text_editor.xview)
+        self.__text_editor['yscrollcommand'] = yscrollbar.set
+        self.__text_editor['xscrollcommand'] = xscrollbar.set
         self.__text_editor.config(bg='#FFF', fg='#000', insertbackground='#000')
+
+        # Readonly textbox - bind key press event
+        self.__text_editor.bind("<Key>", lambda e: "break")
 
     def write(self, *args, **kwargs):
         self.__append_text_editor(*args)
 
     def __append_text_editor(self, txt):
-        self.__text_editor.insert(tk.END, "\n" + txt)
+        self.__text_editor.insert(tk.END, txt)
+        self.update()
+
+
+
 
 
 
