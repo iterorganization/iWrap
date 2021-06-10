ecbd80fb690ff138ab304f0c205f1b19f9199ea0
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-06-10 11:42:53 +0200

    Add Tkinter module to environment.

--- envs/iter-bamboo/10_python_set_env.sh
+++ envs/iter-bamboo/10_python_set_env.sh
@@ -1,13 +1,15 @@
-echo "--------------Module load GCC/9.3.0--------------"
-module load GCC/9.3.0
-echo "--------------Module load Python/3.8.2-GCCcore-9.3.0--------------"
-module load Python/3.8.2-GCCcore-9.3.0
-echo "--------------Module load PyYAML/5.3-GCCcore-9.3.0--------------"
-module load PyYAML/5.3-GCCcore-9.3.0
+echo "--------------Module load GCCcore/10.2.0--------------"
+module load GCCcore/10.2.0
+echo "--------------Module load Python/3.8.6-GCCcore-10.2.0--------------"
+module load Python/3.8.6-GCCcore-10.2.0
+echo "--------------Module load PyYAML/5.3.1-GCCcore-10.2.0--------------"
+module load PyYAML/5.3.1-GCCcore-10.2.0
 echo "--------------Module load lxml/4.5.2-GCCcore-9.3.0--------------"
 module load lxml/4.5.2-GCCcore-9.3.0
 echo "--------------Module load pytest/5.4.3-GCCcore-9.3.0-Python-3.8.2--------------"
 module load pytest/5.4.3-GCCcore-9.3.0-Python-3.8.2
+echo "--------------Module load Tkinter/3.8.6-GCCcore-10.2.0--------------"
+module load Tkinter/3.8.6-GCCcore-10.2.0
 echo "--------------PIP install pylint + pylint-junit"
 python -m pip install pylint
 python -m pip install pylint-junit
