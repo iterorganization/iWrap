86d28cde7903ea6b73d638da12b62a099b18571f
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-06-11 12:17:52 +0200

    Env changes.

--- envs/iter-bamboo/10_python_set_env.sh
+++ envs/iter-bamboo/10_python_set_env.sh
@@ -2,17 +2,19 @@ echo "--------------Module load GCCcore/10.2.0--------------"
 module load GCCcore/10.2.0
 echo "--------------Module load Python/3.8.6-GCCcore-10.2.0--------------"
 module load Python/3.8.6-GCCcore-10.2.0
-echo "--------------Module load PyYAML/5.3.1-GCCcore-10.2.0--------------"
-module load PyYAML/5.3.1-GCCcore-10.2.0
-echo "--------------Module load lxml/4.5.2-GCCcore-9.3.0--------------"
-module load lxml/4.5.2-GCCcore-9.3.0
-echo "--------------Module load pytest/5.4.3-GCCcore-9.3.0-Python-3.8.2--------------"
-module load pytest/5.4.3-GCCcore-9.3.0-Python-3.8.2
 echo "--------------Module load Tkinter/3.8.6-GCCcore-10.2.0--------------"
 module load Tkinter/3.8.6-GCCcore-10.2.0
-echo "--------------PIP install pylint + pylint-junit"
-python -m pip install pylint
-python -m pip install pylint-junit
+echo "--------------Module load PyYAML/5.3.1-GCCcore-10.2.0--------------"
+module load PyYAML/5.3.1-GCCcore-10.2.0
+echo "--------------Module load lxml--------------"
+module load lxml
+echo "--------------Module load pytest--------------"
+module load pytest
+echo "--------------PIP UPGRADE--------------"
+python -m pip install --upgrade pip --disable-pip-version-check --no-warn-script-location
+echo "--------------PIP install pylint + pylint-junit--------------"
+python -m pip install pylint --disable-pip-version-check --no-warn-script-location
+python -m pip install pylint-junit --disable-pip-version-check --no-warn-script-location
 echo "--------------Report Python and pytest version--------------"
 touch python_report.txt
 echo "PYTHON VERSION:" &> python_report.txt
