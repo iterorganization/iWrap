bf84e05636745cb49e6ab9800e2768778f8be48b
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-06-18 09:24:41 +0200

    Upgrade object teardown. Add test_from_dict() test case.

--- tests/test_cases/test_code_description.py
+++ tests/test_cases/test_code_description.py
@@ -1,12 +1,14 @@
 from iwrap.settings.project import ProjectSettings
 from iwrap.settings.serialization import YAMLSerializer
 from tests.misc.services import dict_data
+from typing import Union
+from io import StringIO
 import pytest
 import yaml
 
 
 # Lists of the parameters files and its ids.
-DATA_PARAMS_LIST = ['code_description_data' + "_Case" + str(case).zfill(2) + '.json' for case in range(3)]
+DATA_PARAMS_LIST = ['code_description_data' + "_Case" + str(case).zfill(2) + '.json' for case in range(1)]
 DATA_PARAMS_IDS = ["Case" + str(case).zfill(2) for case, param in enumerate(DATA_PARAMS_LIST)]
 
 
@@ -25,18 +27,41 @@ def yaml_source(dict_ref):
 
 
 @pytest.fixture(scope="function")
-def project_settings(yaml_source):
+def code_description(yaml_source):
     """Populates a ProjectSettings object with data."""
+    # Setup:
     settings = ProjectSettings.get_settings()
-    settings.code_description.load(YAMLSerializer(yaml_source))
-
-    yield settings
-
+    yield settings.code_description
+    # Teardown:
+    ProjectSettings._settings = None
     del settings
 
 
-@pytest.mark.usefixtures("dict_ref", "yaml_source", "project_settings")
+@pytest.mark.usefixtures("dict_ref", "yaml_source", "code_description")
 class TestProjectSettings:
-    def test_code_description_structure(self, project_settings, dict_ref):
+    def test_to_dict(self, code_description, yaml_source, dict_ref):
         """Checks that the CodeDescription dictionary is correctly filled."""
-        assert project_settings.code_description.to_dict() == dict_ref
+        code_description.load(YAMLSerializer(yaml_source))
+        assert code_description.to_dict() == dict_ref
+
+    @pytest.mark.parametrize('param', [1, 2, 3], ids=["attr_count", "attr_compatibility", "val_compatibility"])
+    def test_from_dict(self, param, code_description, dict_ref):
+        code_description.from_dict(dict_ref)
+        if param == 1:
+            assert len(dict_ref) == len(vars(code_description))
+        else:
+            for key in vars(code_description):
+                if param == 2:
+                    assert key in dict_ref
+                else:
+                    value = getattr(code_description, key)
+                    if isinstance(value, Union[str, list, dict, int, bool, None].__args__):
+                        assert value == dict_ref[key]
+                    else:
+                        assert vars(value) == dict_ref[key]
+
+    def test_save(self, code_description, dict_ref):
+        pass
+
+    def test_clear(self, code_description, dict_ref):
+        pass
