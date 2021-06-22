d64cb9ecf97015b5dac6b619c242a04f133e1943
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-06-22 12:47:20 +0200

    Requested changes.

--- envs/iter-bamboo/10_python_set_env.sh
+++ envs/iter-bamboo/10_python_set_env.sh
@@ -1,24 +1,15 @@
-echo "--------------Module load GCC/9.3.0--------------"
+#!/bin/sh
+
+echo "--------------Module load GCC--------------"
 module load GCC
-echo "--------------Module load Python/3.8.2-GCCcore-9.3.0--------------"
+echo "--------------Module load Python--------------"
 module load Python
+echo "--------------PIP install Pylint--------------"
+module load Pylint
 echo "--------------Module load PyYAML--------------"
 module load PyYAML
-echo "--------------Module load lxml/4.5.2-GCCcore-9.3.0--------------"
+echo "--------------Module load lxml--------------"
 module load lxml
-echo "--------------Module load pytest/5.4.3-GCCcore-9.3.0-Python-3.8.2--------------"
-if module avail 2>&1 | grep -c pytest; then
-  module load pytest
-else
-  python -m pip install pytest
-fi
-echo "--------------PIP install pylint + pylint-junit"
-python -m pip install pylint
-python -m pip install pylint-junit
+
 echo "--------------Report Python and pytest version--------------"
-touch python_report.txt
-echo "PYTHON VERSION:" &> python_report.txt
-python --version &>> python_report.txt
-echo "PIP LIST:" &>> python_report.txt
-python -m pip list &>> python_report.txt
-echo "=================================END============================" &>> python_report.txt
+echo "Python version: `python --version`"
