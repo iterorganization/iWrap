e45b84103907a32e07a6fc6328d572682421b196
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-07-05 15:26:44 +0200

    Add relevant shebang with login option.

--- envs/iter-bamboo/00_load_imas_env.sh
+++ envs/iter-bamboo/00_load_imas_env.sh
@@ -1,8 +1,6 @@
 #!/bin/sh --login
 # Set up ITER modules environment
-set -e
 
 # Set up environment
-#source /usr/share/Modules/init/sh
 echo "--------------Module load IMAS--------------"
 module load IMAS
