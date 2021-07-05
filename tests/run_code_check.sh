4febb8dc568a8b55fc1465f2b55b37829bec3b16
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-07-05 15:41:13 +0200

    Add relevant shebang with login option.

--- tests/run_code_check.sh
+++ tests/run_code_check.sh
@@ -3,7 +3,7 @@
 set -e
 my_dir=$(dirname $0)
 wrk_dir=$(dirname $0)/..
-envs_dir=$(dirname $0)/../envs/iter-bamboo
+envs_dir=$(dirname $0)/envs/iter-bamboo
 
 . $envs_dir/00_load_imas_env.sh
 . $envs_dir/10_python_set_env.sh
