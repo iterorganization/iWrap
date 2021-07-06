8c10d3247e05eea8c157d4d3eac0f81a05c6556f
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-07-06 11:40:58 +0200

    Add relevant shebang with login option.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,8 +11,9 @@ else
   echo "++++++++++++++++++++!!!--------------ONBAMBOO--------------!!!++++++++++++++++++++"
   # Source and run scripts with environment vars for CI server
   set -e
-  my_dir=$(dirname $0)
-  envs_dir=$(dirname $0)/../envs/iter-bamboo
+
+  envs_dir=`pwd`/envs/iter-bamboo
+  chmod a+x $envs_dir/00_load_imas_env.sh
   . $envs_dir/00_load_imas_env.sh
   . $envs_dir/10_python_set_env.sh
   . $envs_dir/03_report_module_list.sh
