bbc068c5ac9ace61e73dd65d22e06a0a029ac48f
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-07-06 11:25:03 +0200

    Add relevant shebang with login option.

--- envs/iter-bamboo/task_report_env.sh
+++ envs/iter-bamboo/task_report_env.sh
@@ -2,5 +2,6 @@
 # Task: Setup Environment and report available modules. Save output to artifact.
 set -e
 my_dir=$(dirname $0)
+chmod a+x $my_dir/00_load_imas_env.sh
 . $my_dir/00_load_imas_env.sh
 . $my_dir/02_report_module_avail.sh
