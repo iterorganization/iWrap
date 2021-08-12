e5dc536079b02eb3b4c0c7570990136e9114d56f
Bartek Palak <bartek.palak@man.poznan.pl>
2021-08-12 10:39:31 +0200

    Examples: correction of level2_cpp

--- examples/level2_cpp/native_code/physics_ii.h
+++ examples/level2_cpp/native_code/physics_ii.h
@@ -1,6 +1,4 @@
 #include "UALClasses.h"
 
-#include "codeparam_input_physics.h"
-
 void physics_ii_cpp(IdsNs::IDS::equilibrium in_equilibrium, IdsNs::IDS::equilibrium& out_equilibrium, IdsNs::codeparam_t codeparam, int* status_code, char** status_message);
 
