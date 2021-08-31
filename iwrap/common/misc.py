67bf98f9bd8811b7b85738c83bf31207d935bfe0
Bartek Palak <bartek.palak@man.poznan.pl>
2021-08-31 14:39:57 +0200

    Dictionarizable: changing the way, object attributes/properties are listed

--- iwrap/common/misc.py
+++ iwrap/common/misc.py
@@ -1,8 +1,16 @@
+import inspect
 from abc import ABC, abstractmethod
 from typing import List, Any, Dict
 
 
 class Dictionarizable( ABC ):
+
+    def _list_attributes(self):
+        members_list = inspect.getmembers(self,  lambda member: not inspect.isroutine(member))
+        members_list = filter( lambda member: not member[0].startswith( "_" ), members_list )
+
+        return members_list
+
 
     @abstractmethod
     def from_dict(self, dictionary: Dict[str, Any]) -> None:
@@ -28,9 +36,9 @@ class Dictionarizable( ABC ):
         Returns
             Dict[str, Any]: Dictionary containing object data
         """
-        obj_as_dict_ = vars( self )
+        obj_as_dict_ = self._list_attributes()
         dict_ = {}
-        for key, value in obj_as_dict_.items():
+        for key, value in self._list_attributes():
             if isinstance( value, Dictionarizable ):
                 dict_[key] = value.to_dict()
             elif isinstance( value, List ):
