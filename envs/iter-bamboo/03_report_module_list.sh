9550d4a55a129abe213bba86b6d8149f6329e42c
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-09-14 10:13:25 +0200

    Removed explicit load of the module Python an PyYaml.

--- envs/iter-bamboo/03_report_module_list.sh
+++ envs/iter-bamboo/03_report_module_list.sh
@@ -2,5 +2,7 @@
 
 # MODULES LIST
 touch modules_list.txt
-module list &> modules_list.txt
+touch pip_list.txt
+module list -t &> modules_list.txt
+python3 -m pip list &> pip_list.txt
 echo "=================================END============================" &>> modules_list.txt
