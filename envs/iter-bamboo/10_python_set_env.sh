aaaf61c772e6b10794e661ddb23e72d269813e41
Smukowski Maciej <msmukowski@man.poznan.pl>
2021-09-22 22:08:38 +0200

    Specified the version of loaded modules.

--- envs/iter-bamboo/10_python_set_env.sh
+++ envs/iter-bamboo/10_python_set_env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh --login
 
 echo "--------------PIP install Pylint--------------"
-module load Pylint
+module load Pylint/2.7.4-GCCcore-10.2.0
 echo "--------------Module load lxml--------------"
-module load lxml
+module load lxml/4.6.2-GCCcore-10.2.0
 
 echo "--------------Report Python and pytest version--------------"
 echo "Python version: `python --version`"
