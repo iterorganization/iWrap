c50f06b842d43e4945290a20cae8dd11d5f1791f
Bartek Palak <bartek.palak@man.poznan.pl>
2021-10-08 09:14:22 +0200

    New example: code lifetime (Fortran)

---
+++ examples/code_lifecycle/native_code/code_lifecycle.f90
@@ -0,0 +1,102 @@
+module mod_code_lifecycle
+
+contains
+
+    !
+    !    INITIALISATION SUBROUTINE
+    !
+    subroutine init_code (xml_parameters, status_code, status_message)
+        use ids_schemas, only: ids_parameters_input
+        implicit none
+        type(ids_parameters_input) :: xml_parameters
+        integer, intent(out) :: status_code
+        character(len=:), pointer, intent(out) :: status_message
+
+
+        ! Setting status to SUCCESS
+        status_code = 0
+        allocate(character(50):: status_message)
+        status_message = 'OK'
+
+        write(*,*) '======================================='
+        write(*,*) 'Code lifecycle: INITIALISATION called'
+        write(*,*) '======================================='
+
+    end subroutine init_code
+
+
+    !
+    !    FINALISATION SUBROUTINE
+    !
+    subroutine clean_up(status_code, status_message)
+        implicit none
+        integer, intent(out) :: status_code
+        character(len=:), pointer, intent(out) :: status_message
+
+        ! Setting status to SUCCESS
+        status_code = 0
+        allocate(character(50):: status_message)
+        status_message = 'OK'
+
+        write(*,*) '======================================='
+        write(*,*) 'Code lifecycle: FINALISATION called'
+        write(*,*) '======================================='
+
+    end subroutine clean_up
+
+    !
+    !    MAIN SUBROUTINE
+    !
+    subroutine code_lifecycle(equilibrium_in, equilibrium_out, codeparam, status_code, status_message)
+        use ids_schemas, only: ids_equilibrium, ids_parameters_input, ids_is_valid
+        use ids_routines, only: ids_copy
+
+        implicit none
+
+        type(ids_equilibrium):: equilibrium_in,equilibrium_out
+        type(ids_parameters_input) :: codeparam
+        integer, intent(out) :: status_code
+        character(len=:), pointer, intent(out) :: status_message
+
+        ! INITIALISATION OF ERROR FLAG
+        status_code = 0
+
+        ! INITIAL DISPLAY
+        write(*,*) ' '
+        write(*,*) '======================================='
+        write(*,*) 'START OF PHYSICS CODE'
+
+        ! CHECK IF INPUT IDS IS VALID
+        if (ids_is_valid(equilibrium_in%ids_properties%homogeneous_time).eqv..True. &
+            .and.size(equilibrium_in%time)>0) then
+
+            ! COPY THE INPUT IDS IN THE OUTPUT IDS
+            call ids_copy(equilibrium_in, equilibrium_out)
+
+            ! MANDATORY FLAG (UNIFORM TIME HERE)
+            equilibrium_out%ids_properties%homogeneous_time = 1
+            allocate(equilibrium_out%code%name(1))   ! For a string of 132 characters max.
+
+            equilibrium_out%code%name(1)   = 'EXAMPLE: code_lifecycle'
+            allocate(equilibrium_out%code%version(1))   ! For a string of 132 characters max.
+
+            equilibrium_out%code%version(1)   = '1.0'
+            allocate(equilibrium_out%code%parameters(1))   ! For a string of 132 characters max.
+            equilibrium_out%code%parameters(1) = 'EXAMPLE: code_lifecycle: NO PARAMETERS'
+            allocate(equilibrium_out%code%output_flag(1))
+            equilibrium_out%code%output_flag(1) = 0   ! Integer output flag, 0 means the run was successful and can be used in the rest of the workflow, <0 means failure
+
+        else
+            ! ERROR IF THE CODE DOES NOT COMPLETE TO THE END
+            status_code = -1
+            allocate(character(50):: status_message)
+            status_message = 'Error in physics_ii: input IDS not valid'
+        endif
+
+        ! FINAL DISPLAY
+        write(*,*) 'END OF PHYSICS CODE'
+        write(*,*) '======================================='
+        write(*,*) ' '
+
+        end subroutine code_lifecycle
+end module mod_code_lifecycle
---
+++ examples/code_lifecycle/workflow.py
@@ -0,0 +1,90 @@
+import sys
+
+import imas,os
+
+from code_lifecycle.actor import code_lifecycle
+from code_lifecycle.common.runtime_settings import RunMode, DebugMode
+
+
+
+class ExampleWorkflowManager:
+
+    def __init__(self):
+
+        self.actor_code_lifecycle = code_lifecycle()
+        self.input_entry = None
+        self.output_entry = None
+
+    def init_workflow(self):
+        # INPUT/OUTPUT CONFIGURATION
+        shot                = 131024
+        run_in              = 1
+        input_user_or_path  = 'public'
+        input_database      = 'iter'
+        run_out             = 10
+        output_user_or_path = os.getenv('USER')
+        output_database     = input_database
+
+        # OPEN INPUT DATAFILE TO GET DATA FROM IMAS SCENARIO DATABASE
+        print('=> Open input datafile')
+        self.input_entry = imas.DBEntry(imas.imasdef.MDSPLUS_BACKEND,input_database,shot,run_in,input_user_or_path)
+        self.input_entry.open()
+        
+        # CREATE OUTPUT DATAFILE
+        print('=> Create output datafile')
+        self.output_entry = imas.DBEntry(imas.imasdef.MDSPLUS_BACKEND,output_database,shot,run_out,output_user_or_path)
+        self.output_entry.create()
+
+        # # # # # # # # Initialization of ALL actors  # # # # # # # #
+        actor_run_mode = os.getenv( 'ACTOR_RUN_MODE', 'NORMAL')
+        if actor_run_mode == 'STANDALONE':
+            print('Running STANDALONE version.')
+            self.actor_code_lifecycle.runtime_settings.run_mode = RunMode.STANDALONE
+
+        self.actor_code_lifecycle.initialize() 
+    
+    def execute_workflow(self):
+        # READ INPUT IDSS FROM LOCAL DATABASE
+        time_slice          = 200.
+        print('=> Read input IDSs')
+        input_equilibrium = self.input_entry.get_slice('equilibrium', time_slice, 1)
+
+        # EXECUTE PHYSICS CODE
+        print('=> Execute physics code')
+
+        output_equilibrium = self.actor_code_lifecycle(input_equilibrium)
+        
+        
+        # SAVE IDSS INTO OUTPUT FILE
+        print('=> Export output IDSs to local database')
+        self.output_entry.put(output_equilibrium)
+        print('Done exporting.')
+
+
+    def end_workflow(self):
+        
+        # Finalize ALL actors 
+        self.actor_code_lifecycle.finalize()
+
+        output_ids = self.output_entry.get('equilibrium')
+
+        with open( 'wf_output.txt', 'w' ) as file:
+            file.write( str(output_ids.time) )
+        
+        #other finalizastion actions
+        self.input_entry.close()
+        self.output_entry.close()
+
+
+
+manager = ExampleWorkflowManager()
+
+manager.init_workflow()
+manager.execute_workflow()
+manager.end_workflow()
+
+
+
+
+
+
