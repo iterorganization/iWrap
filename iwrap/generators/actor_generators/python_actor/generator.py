223030a9561a1cf0d6a61b88e37ae789b5aac6b7
Natalia Kraszewska <nkraszewska@man.poznan.pl>
2021-11-09 13:44:21 +0100

    Change install_dir access from public to private

--- iwrap/generators/actor_generators/python_actor/generator.py
+++ iwrap/generators/actor_generators/python_actor/generator.py
@@ -55,7 +55,7 @@ class PythonActorGenerator(ActorGenerator):
         self.install_dir: str = None
 
     def initialize(self):
-        install_dir =  ProjectSettings.get_settings().actor_description.install_dir
+        install_dir =  ProjectSettings.get_settings().actor_description._install_dir
         if not install_dir:
             install_dir = PlatformSettings().default_directories.actor_default_install_dir
         self.install_dir: str = str(Path(install_dir, ProjectSettings.get_settings().actor_description.actor_name))
