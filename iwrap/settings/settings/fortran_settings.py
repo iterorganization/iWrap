c0666e2d8902fdf2d7867a876e37029f7685aa6f
Natalia Kraszewska <nkraszewska@man.poznan.pl>
2021-11-18 15:27:59 +0100

    Remove MPI and Batch classes, update paths

--- iwrap/settings/settings/fortran_settings.py
+++ iwrap/settings/settings/fortran_settings.py
@@ -12,88 +12,6 @@ class AbstractLanguageSpecificSettings( SettingsBaseClass, ABC ):
     # Class logger
     __logger = logging.getLogger(__name__ + "." + __qualname__)
 
-    pass
-
-
-
-class MPI( SettingsBaseClass ):
-
-    @property
-    def mpi_compiler_cmd(self):
-        return self._mpi_compiler_cmd
-
-    @mpi_compiler_cmd.setter
-    def mpi_compiler_cmd(self, value):
-        self._mpi_compiler_cmd = value if value != "None" else None
-
-    @property
-    def mpi_runner(self):
-        return self._mpi_runner
-
-    @mpi_runner.setter
-    def mpi_runner(self, value):
-        self._mpi_runner = value if value != "None" else None
-
-    def __init__(self):
-        self._mpi_compiler_cmd = ''
-        self._mpi_runner = ''
-
-    def validate(self, engine: Engine, project_root_dir: str) -> None:
-
-        if self.mpi_runner and not self.mpi_compiler_cmd:
-            raise ValueError('MPI runner command is not set')
-
-        if self.mpi_compiler_cmd and not self.mpi_runner:
-            raise ValueError('MPI compiler is not set')
-
-    def clear(self):
-        self.__init__()
-
-    def from_dict(self, dictionary: dict):
-        super().from_dict( dictionary )
-
-    def to_dict(self, resolve_path: bool = False, make_relative: str = False, project_root_dir: str = None) -> Dict[
-        str, Any]:
-        return super().to_dict()
-
-
-class Batch( SettingsBaseClass ):
-    @property
-    def batch_runner(self):
-        return self._batch_runner
-
-    @batch_runner.setter
-    def batch_runner(self, value):
-        self._batch_runner = value if value != "None" else None
-
-    @property
-    def batch_default_queue(self):
-        return self._batch_default_queue
-
-    @batch_default_queue.setter
-    def batch_default_queue(self, value):
-        self._batch_default_queue = value if value != "None" else None
-
-    def __init__(self):
-        self._batch_runner = ''
-        self._batch_default_queue = ''
-
-    def validate(self, engine: Engine, project_root_dir: str) -> None:
-        if self.batch_runner and not self.batch_runner:
-            raise ValueError('Batch runner command is not set')
-
-        if self.batch_default_queue and not self.batch_default_queue:
-            raise ValueError('Batch queue is not set')
-
-    def clear(self):
-        self.__init__()
-
-    def from_dict(self, dictionary: dict):
-        super().from_dict( dictionary )
-
-    def to_dict(self, resolve_path: bool = False, make_relative: str = False, project_root_dir: str = None) -> Dict[str, Any]:
-        return super().to_dict()
-
 
 class ExtraLibraries( SettingsBaseClass ):
 
@@ -122,7 +40,6 @@ class ExtraLibraries( SettingsBaseClass ):
         ret_dict = super().to_dict(resolve_path, make_relative, project_root_dir)
 
         if resolve_path:
-            # include_path
             resolved_libs = []
             for library in self.path_defined or []:
                 __path = utils.resolve_path( library, project_root_dir )
@@ -144,11 +61,18 @@ class FortranSpecificSettings( AbstractLanguageSpecificSettings ):
     def open_mp_switch(self, value):
         self._open_mp_switch = value if value != "None" else None
 
+    @property
+    def mpi_compiler_cmd(self):
+        return self._mpi_compiler_cmd
+
+    @mpi_compiler_cmd.setter
+    def mpi_compiler_cmd(self, value):
+        self._mpi_compiler_cmd = value if value != "None" else None
+
     def __init__(self):
         self.compiler_cmd = ''
-        self.mpi = MPI()
-        self.batch = Batch()
         self._open_mp_switch = False
+        self._mpi_compiler_cmd = ''
         self.extra_libraries = ExtraLibraries()
 
     def validate(self, engine: Engine, project_root_dir: str) -> None:
@@ -157,20 +81,6 @@ class FortranSpecificSettings( AbstractLanguageSpecificSettings ):
         if not self.compiler_cmd:
             raise ValueError( 'Compiler to be used is not set!' )
         # TODO Validate compiler against platform settings
-
-        # # include_path
-        # if not self.include_path:
-        #     raise ValueError( 'Path to include/module file is not set!' )
-        #
-        # __path = utils.resolve_path( self.include_path, project_root_dir)
-        # if not Path(__path).exists():
-        #     raise ValueError( f'Path to include/module file is not valid! {str( __path )}' )
-
-        # mpi
-        self.mpi.validate(engine, project_root_dir)
-
-        # mpi
-        self.batch.validate(engine, project_root_dir)
 
         # open_mp
         # TODO validate open mp
@@ -186,13 +96,4 @@ class FortranSpecificSettings( AbstractLanguageSpecificSettings ):
 
     def to_dict(self, resolve_path: bool = False, make_relative:str = False, project_root_dir:str = None) -> Dict[str, Any]:
         return super().to_dict()
-    #     ret_dict = super().to_dict(resolve_path, make_relative, project_root_dir)
-    #
-    #     if resolve_path:
-    #         # include_path
-    #         __path = utils.resolve_path( self.include_path, project_root_dir )
-    #         ret_dict.update( {'include_path': __path} )
-    #
-    #     return ret_dict
 
-    # pass
