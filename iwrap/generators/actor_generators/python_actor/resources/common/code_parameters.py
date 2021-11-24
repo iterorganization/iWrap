f1c72686772fd491c727e2dc6c1563c291607a1d
Natalia Kraszewska <nkraszewska@man.poznan.pl>
2021-11-24 16:07:32 +0100

    Add get/set xml value logic

--- iwrap/generators/actor_generators/python_actor/resources/common/code_parameters.py
+++ iwrap/generators/actor_generators/python_actor/resources/common/code_parameters.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
-
+from xml.etree import ElementTree as et
+import re
 from lxml import etree
 
 
@@ -17,6 +18,42 @@ class CodeParameters:
     def parameters(self):
         return self.__parameters_str
 
+    def is_leaf(self, node):
+        return len(list(node)) < 1
+
+    def get_tree_info(self, path):
+        pattern = r'\((\d)\)'
+
+        # get current node and index
+        split_path = path.split('/')
+        current_node = split_path[1]
+        index = 0
+
+        # if index in current node
+        if re.search(pattern, current_node):
+            index = int(re.search(r'\d+', current_node).group()) - 1
+            current_node = current_node[:-3]
+
+        # remove current node from path and run function again
+        path = "/".join(split_path[1:])
+
+        return path, index, current_node
+
+    def get_xml_value(self, path, tree):
+        if self.is_leaf(tree):
+            return tree.text
+
+        path, index, current_node = self.get_tree_info(path)
+        self.get_xml_value(path, tree.findall(current_node)[index])
+
+    def set_xml_value(self, path, tree, value):
+        if self.is_leaf(tree):
+            tree.text = value
+            return
+
+        path, index, current_node = self.get_tree_info(path)
+        self.set_xml_value(path, tree.findall(current_node)[index], value)
+
     def get_parametr_value(self, path_to_node:str) -> str:
         if not self.__default_parameters_path:
             return
@@ -24,8 +61,9 @@ class CodeParameters:
         if not self.__parameters_str:
             self.initialize()
 
-        # add  operations on XML tree
-        return 'PARAMETER VALUE'
+        tree = et.ElementTree(et.fromstring(self.__parameters_str))
+        root = tree.getroot()
+        return self.get_xml_value(path_to_node, root)
 
     def set_parametr_value(self, path_to_node:str, value) -> None:
         if not self.__default_parameters_path:
@@ -34,6 +72,9 @@ class CodeParameters:
             self.initialize()
 
         # add  operations on XML tree
+        tree = et.ElementTree(et.fromstring(self.__parameters_str))
+        root = tree.getroot()
+        self.set_xml_value(path_to_node, root, value)
 
     def __init__(self, default_parameters_path:str, schema_path:str):
 
