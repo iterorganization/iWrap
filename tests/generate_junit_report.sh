fb712ad87a7aaadf56ab3c6b5bdda904bbc79da8
Maciej Smukowski <msmukowski@man.poznan.pl>
2021-12-10 14:37:34 +0100

    Added generation of testcases in the report.

--- tests/generate_junit_report.sh
+++ tests/generate_junit_report.sh
@@ -2,8 +2,11 @@
 
 declare -i tests_count=0
 declare -i tests_failures=0
+declare test_command
+declare test_cases
 declare -r project_root=$(pwd)
 declare -r junit_report_template=$project_root/tests/test_data/junit_report_template.in
+declare -r junit_report_testcase_template=$project_root/tests/test_data/junit_report_testcase_template.in
 declare -r report_destination=$project_root/reports/report.xml
 
 
@@ -18,18 +21,26 @@ chmod a+x ./set-iter.sh
 # Look for a tests artifacts with exit codes values
 for file in $project_root/reports/*_exit_code.txt; do
     tests_count=$tests_count+1
-    
+
+    test_command=$(echo $file | cut -d'_' -f 2)
+    test_cases=$(echo $(<$junit_report_testcase_template) |
+                (sed -e "s;__NAME__;$TEST_DIR_NAME;g"\
+                     -e "s;__TESTCOMMAND__;$test_command;"))$test_cases
+
     if [ $(<$file) -ne 0 ]; then
         tests_failures=$tests_failures+1
     fi
 done
 
+echo -e "test cases outcome:\n\t$test_cases"
+
 echo -e "\n\t\033[1;33mGENERATING A TEST REPORT\033[0m\n"
 sed -e "s;__ERRORS__;0;" \
     -e "s;__FAILURES__;$tests_failures;" \
-    -e "s;__NAME__;TEST $TEST_DIR_NAME;" \
+    -e "s;__NAME__;$TEST_DIR_NAME;" \
     -e "s;__TESTS__;$tests_count;" \
     -e "s;__TIME__;$(date +"%Y-%m-%dT%H:%M:%S.%N");" \
+    -e "s;__TEST_CASES__;$test_cases;" \
     $junit_report_template > $report_destination
 
 echo -e "\t\033[0;33mReport saved in:\n\t\033[1;35m ${report_destination}\033[0m\n"
