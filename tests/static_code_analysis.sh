2ddca773586e1ad965bc22b6e662064391ed70c0
Maciej Smukowski <msmukowski@man.poznan.pl>
2022-10-26 09:10:51 +0200

    add: wait for virtualenv to finish activating

--- tests/static_code_analysis.sh
+++ tests/static_code_analysis.sh
@@ -3,10 +3,28 @@
 set -e
 
 envs_dir=`pwd`/envs/iter-bamboo
+echo "Environement scripts path: $envs_dir"
 chmod a+x $envs_dir/00_load_imas_env.sh
 . $envs_dir/00_load_imas_env.sh
 . $envs_dir/10_python_set_env.sh
-. ./venv/bin/activate
+. $envs_dir/../../venv/bin/activate
+BACK_PID=$!
+max_retry=20
+retry=0
+while [ ${retry} -lt ${max_retry} ]; do
+	if [ kill -0 $BACK_PID 2>/dev/null ]; then
+		echo "Virtualenv ready!"
+		sleep 1
+	else
+		echo "Waiting for virtualenv to activate! Retry: $retry"
+		venv_path=$(realpath $envs_dir/../../venv)
+		echo "Path to virtualenv: $venv_path"
+		ls $venv_path
+		(( retry = retry + 1 ))
+		sleep 1
+	fi
+done
+
 echo -e Python virtualenv active: `which python` 
 . $envs_dir/03_report_module_list.sh
 
