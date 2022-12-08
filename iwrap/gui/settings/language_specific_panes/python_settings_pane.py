1ee2b99ead74f8f3c6c891ab6bf66dd92633bfad
Bartek Palak <bartek.palak@man.poznan.pl>
2022-12-08 11:08:07 +0100

    Correction GUI issues while switching implementation languages  [IMAS-4436]

--- iwrap/gui/settings/language_specific_panes/python_settings_pane.py
+++ iwrap/gui/settings/language_specific_panes/python_settings_pane.py
@@ -11,7 +11,7 @@ class PythonPane( ttk.Frame, IWrapPane ):
 
     def __init__(self, master=None, language=None):
         super().__init__( master )
-        ttk.Label( self, text="Place for SPECIFIC Python settings", borderwidth=1, relief="solid" ).pack( fill=tk.BOTH, expand=1 )
+
 
 
     def update_settings(self):
