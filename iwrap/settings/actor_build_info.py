7b3eefd5c4117e49b1ac9ec657160f56025d0a78
wasikj <jwasik@man.poznan.pl>
2023-03-16 06:50:02 +0100

    Remove to_dict(), from_dict() in actor_build_info. [IMAS-4460]

--- iwrap/settings/actor_build_info.py
+++ iwrap/settings/actor_build_info.py
@@ -12,20 +12,3 @@ class ActorBuildInfo(Dictionarizable):
         self.imas_prefix = getenv('IMAS_PREFIX')
         self.al_version = getenv('UAL_VERSION')
         self.generation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    def from_dict(self, dictionary: Dict[str, Any]) -> None:
-        self.iwrap_version = dictionary.get('iwrap_version', 'not specified')
-        self.imas_version = dictionary.get('imas_version', 'not specified')
-        self.imas_prefix = dictionary.get('imas_prefix', 'not specified')
-        self.imas_prefix = dictionary.get('al_version', 'not specified')
-        self.generation_date = dictionary.get('generation_date', 'not specified')
-
-    def to_dict(self, resolve_path: bool = False, make_relative: str = False, project_root_dir: str = None) -> Dict[str, Any]:
-        dict = {
-            'iwrap_version': self.iwrap_version,
-            'imas_version': self.imas_version,
-            'imas_prefix': self.imas_prefix,
-            'al_version': self.al_version,
-            'generation_date': self.generation_date
-        }
-        return dict
