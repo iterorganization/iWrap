f3e0778558f43df9bea32c3c73b3064ca0e27804
Bartek Palak <bartek.palak@man.poznan.pl>
2023-08-03 08:24:23 +0200

    Changes related to a new pylint version API [IMAS-4737]

--- tests/static_code_analysis.sh
+++ tests/static_code_analysis.sh
@@ -9,6 +9,7 @@ activate_venv () {
 }
 
 echo "Environement scripts path: $envs_dir"
+echo "VENV path: $venv_path"
 chmod a+x $envs_dir/00_load_imas_env.sh
 source $envs_dir/00_load_imas_env.sh
 source $envs_dir/10_python_set_env.sh
@@ -35,9 +36,11 @@ if [ $venv_status -ne 0 ]; then
 	exit $venv_status
 fi
 
-echo -e Python virtualenv active: `which ${venv_path/bin/python}` 
+echo -e Python virtualenv active: `which ${venv_path}/bin/python`
 source $envs_dir/03_report_module_list.sh
 
 # Run pylint code check
 echo "~~~~~====================PYLINT CODE CHECK====================~~~~~"
+# to be used with pylint 2.17.4
+#$venv_path/bin/python -m pylint --errors-only --output-format=pylint_junit.JUnitReporter --output=pylint.xml iwrap
 $venv_path/bin/python -m pylint -E --output-format=pylint_junit.JUnitReporter iwrap > pylint.xml
