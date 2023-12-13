576379a316fb4cd679648795b2018561dca09b99
wasikj <jwasik@man.poznan.pl>
2023-12-13 07:26:26 +0100

    Update tooltips. [IMAS-4570]

--- iwrap/gui/settings/tooltip.py
+++ iwrap/gui/settings/tooltip.py
@@ -56,13 +56,15 @@ class ToolTip(object):
             tw.destroy()
 
 tooltip_dict ={
-    'actor_name':'The arbitrary, user defined name of the actor. It determines e.g. : the name of class to be generated and directory where actor will be put.',
+    'actor_name':'The arbitrary, user defined name of the actor. It determines e.g. : the name of class to be generated and directory where actor will be put.\n\n'
+                 'example: my_actor',
     'actor_type':'Value: ‘python’ (currently only python type has been implemented)',
     'programming_language':'Language of physics code. One of predefined values: Fortran, CPP.',
-    'data_dictionary_compliant':'Oldest known version of Data Directory compatible with actor.',
+    'data_dictionary_compliant':'Oldest known version of Data Directory compatible with actor.\n\n'
+                                'example: 3.39.0',
     'root_dir':'The root directory for ALL relative paths placed in code description. A relative path leading from YAML file location to actor project root dir.\n\n'
                'Default: ‘.‘',
-    'data_type':'Data type handled by the physics code. value: ‘legacy’ (currently only ‘Legacy IDS’ type has been implemented).',
+    'data_type':'Data type handled by the physics code.\nvalue: ‘legacy’ (currently only ‘Legacy IDS’ type has been implemented).',
     'code_path':'Path to system library (C, C++, Fortran) , script (Python), etc., containing the physics code, including methods/subroutines to be run. Relative paths start at "Root dir" directory.\n\n'
                 'examples:\n'
                 'C++/Fortran: ‘/path/to/code/lib/libcode.a’',
@@ -70,12 +72,24 @@ tooltip_dict ={
                 'examples:\n'
                    'C++: ‘/path/to/code/include/code.h’\n'
                    'Fortran: ‘/path/to/code/include/code.mod',
-    'init':'Name of user method / subroutine to be called. It is used, usually, to set up the native code, however subroutine may contain any arbitrary actions.',
-    'main':'Name of user method / subroutine to be called. It is main method of your physics code.',
-    'finalize':'Name of user method / subroutine to be called. It is used, usually, to clean up the native code, however subroutine may contain any arbitrary actions.',
-    'get_status':'Name of user method / subroutine to be called. It is used, usually, to get current state of native code, however subroutine may contain any arbitrary actions.',
-    'set_status':'Name of user method / subroutine to be called. It is used, usually, to set current state of native code, however subroutine may contain any arbitrary actions.',
-    'get_timestamp': 'Name of user method / subroutine to be called. It is used, usually, to get timestamp of native code, however subroutine may contain any arbitrary actions.',
+    'init':'Name of user method / subroutine to be called. It is used, usually, to set up the native code, however subroutine may contain any arbitrary actions.\n'
+           'IMPORTANT! method name must be the same as in native code.\n\n'
+           'example: my_init_method',
+    'main':'Name of user method / subroutine to be called. It is main method of your physics code.\n'
+           'IMPORTANT! method name must be the same as in native code.\n\n'
+           'example: my_main_method',
+    'finalize':'Name of user method / subroutine to be called. It is used, usually, to clean up the native code, however subroutine may contain any arbitrary actions.\n'
+           'IMPORTANT! method name must be the same as in native code.\n\n'
+           'example: my_finalize_method',
+    'get_status':'Name of user method / subroutine to be called. It is used, usually, to get current state of native code, however subroutine may contain any arbitrary actions.\n'
+           'IMPORTANT! method name must be the same as in native code.\n\n'
+           'example: my_get_status_method',
+    'set_status':'Name of user method / subroutine to be called. It is used, usually, to set current state of native code, however subroutine may contain any arbitrary actions.\n'
+           'IMPORTANT! method name must be the same as in native code.\n\n'
+           'example: my_set_status_method',
+    'get_timestamp': 'Name of user method / subroutine to be called. It is used, usually, to get timestamp of native code, however subroutine may contain any arbitrary actions.\n'
+           'IMPORTANT! method name must be the same as in native code.\n\n'
+           'example: my_get_timestamp_method',
     'parameters_path':'Path to XML file containing user defined parameters of the physics model.\n\n'
        'example: ‘./code_parameters/parameters.xml’',
     'schema_path':'Path to XSD file contains schema of XML parameters, enabling its validation.',
@@ -86,8 +100,7 @@ tooltip_dict ={
     'schema_file':'Path to XSD file contains schema of XML parameters, enabling its validation.\n\n'
                   'example: ‘./code_parameters/parameters.xsd’',
     'argument_window.Name':'User defined argument name. Can be used in actor\'s main method call.\n\n'
-                           'example: ’equilibrium00’\n'
-                           'example argument\'s name usage: ’result = my_actor(equilibrium00=some_ids_variable)’',
+                           'example: ’equilibrium00’',
     'argument_window.Input':'Determines if given argument is input or output one.',
     'argument_window.Type':'A type of an IDS argument.',
     'compiler_cmd':'Name/vendor of the compiler command used to compile native codes.\n\n'
