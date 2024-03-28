b946e9508662ce486876dedea4183f4e7576b672
Aleksander Wiszniowski <awiszniowski@man.poznan.pl>
2024-03-28 10:26:21 +0100

    feat: add CI/CD files

---
+++ envs/iter-bamboo/20_prepare_env_for_tutorials.sh
@@ -0,0 +1,11 @@
+#!/bin/sh --login
+
+module unload IMAS/3.39.0-4.11.7-foss-2020b
+module load IMAS/3.39.0-5.0.0-foss-2020b
+echo  "-----------------Loaded IMAS 3.39.0-5.0.0 -----------------"
+
+iwrap --version
+echo "-----------------iWrap version-----------------"
+
+./docs/scripts/create_db
+echo "-----------------IMAS DB created------------"
