4f8a45d7f3f3ca3199b4bab04ab1bbb20fd3f2e2
Palak Bartek EXT <bartek@man.poznan.pl>
2024-06-28 13:55:04 +0200

    Updating CI env to use 2023b toolchain

--- envs/iter-bamboo/00_load_imas_env.sh
+++ envs/iter-bamboo/00_load_imas_env.sh
@@ -14,10 +14,16 @@ if [ "$COMPILER_VENDOR" == "intel" ]; then  # INTEL
   export MPIFC="mpiifort"
 else
 # GFORTRAN
-  module load XMLlib/3.3.1-GCC-10.2.0
-  module load lxml/4.6.2-GCCcore-10.2.0
-  module load IMAS/3.39.0-5.0.0-foss-2020b
-  module load JPype1/1.4.1-foss-2020b-Java-11
+  module load IMAS/3.41.0-4.11.10-foss-2023b
+
+  module load lxml/4.9.3-GCCcore-13.2.0
+  module load JPype1/1.5.0-gfbf-2023b
+  module load XMLlib/3.3.1-GCC-13.2.0
+
+  module load json-fortran/8.5.2-GCC-13.2.0
+  module load JsonCpp/1.9.5-GCCcore-13.2.0
+  module load f90nml/1.4.4-GCCcore-13.2.0
+
   export CXX="g++"
   export FC="gfortran"
   export MPICXX="mpicxx"
