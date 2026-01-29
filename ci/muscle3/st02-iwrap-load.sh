52eea1262f51c50f53f8156d817a1be9aa863c3c
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2026-01-29 10:47:29 +0100

    removed setting TEST_DIR

--- ci/muscle3/st02-iwrap-load.sh
+++ ci/muscle3/st02-iwrap-load.sh
@@ -6,11 +6,11 @@ export IWRAP_HOME=$(realpath "$(dirname ${BASH_SOURCE})/../..")
 export PATH=${IWRAP_HOME}/bin:${PATH}
 
 export PYTHONPATH=${IWRAP_HOME}:${PYTHONPATH}
-export TESTS_DIR="${IWRAP_HOME}/tests"
+
 
 echo "IWRAP_HOME: $IWRAP_HOME"
 echo "PATH: $PATH"
 echo "PYTHONPATH: $PYTHONPATH"
-echo "TESTS_DIR: $TESTS_DIR"
+
 echo "IWRAP setup completed successfully"
 
--- envs/common/01_set_iwrap_env.sh
+++ envs/common/01_set_iwrap_env.sh
@@ -2,10 +2,6 @@
 
 export PATH=${IWRAP_HOME}/bin:${PATH}
 
-if [[ ! -n $AL_VERSION ]]; then
-    export AL_VERSION=$UAL_VERSION
-fi
-
 export PYTHONPATH=${IWRAP_HOME}:${PYTHONPATH}
 export TESTS_DIR="${IWRAP_HOME}/tests"
 
