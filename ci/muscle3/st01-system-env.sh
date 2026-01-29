42e9068e41e23555ec61939984f3203d4dbb9095
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2026-01-29 11:33:32 +0100

    added makefile

--- ci/muscle3/st01-system-env.sh
+++ ci/muscle3/st01-system-env.sh
@@ -46,8 +46,8 @@ fi
 
 export AL_MAJOR="${AL_VERSION%.*.*}"
 
-# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# export TESTS_DIR=$( cd -- "${SCRIPT_DIR}/../../tests" &> /dev/null && pwd )
+export TESTS_DIR=$( cd -- "${SCRIPT_DIR}/../../tests" &> /dev/null && pwd )
 
-# echo TESTS_DIR: $TESTS_DIR
+echo TESTS_DIR: $TESTS_DIR
---
+++ tests/muscle3/pytests.py
@@ -0,0 +1,4 @@
+#!/bin/env python3
+
+
+print("I am placeholder for a future tests!")
