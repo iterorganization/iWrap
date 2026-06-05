8764cae560f279a65f05aa2672bc539940fa4aa4
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2026-06-05 12:58:56 +0200

    added package data and updated docs/req

--- envs/common/22_build_iwrap_documentation.sh
+++ envs/common/22_build_iwrap_documentation.sh
@@ -8,7 +8,7 @@ echo "-----------------IMAS DB created------------"
 
 . `pwd`/venv/bin/activate
 # . $envs_dir/21_install_python_packages_for_tutorial.sh
-python -m pip install --ignore-installed --requirement ./docs/requirements.txt
+python -m pip install --ignore-installed ".[docs]"
 
 cd docs/
 
