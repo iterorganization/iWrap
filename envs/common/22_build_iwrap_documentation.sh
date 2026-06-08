a6761feec114c3f7d6de47980db2c64ba461657f
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2026-06-08 10:43:25 +0200

    add debug prints

--- envs/common/22_build_iwrap_documentation.sh
+++ envs/common/22_build_iwrap_documentation.sh
@@ -9,6 +9,14 @@ echo "-----------------IMAS DB created------------"
 . `pwd`/venv/bin/activate
 # . $envs_dir/21_install_python_packages_for_tutorial.sh
 
+echo "PIP_USER: $PIP_USER"
+echo "PYTHONUSERBASE: $PYTHONUSERBASE"
+pip config list
+
+which python
+python -m pip -V
+python -m site
+
 python -m pip install --no-user --ignore-installed ".[docs]"
 
 cd docs/
