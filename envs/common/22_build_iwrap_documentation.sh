612949ed670e842f8afcf7faaa69eabfc3a3c520
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2026-06-08 10:33:27 +0200

    do not install packages in bamboo local dir

--- envs/common/22_build_iwrap_documentation.sh
+++ envs/common/22_build_iwrap_documentation.sh
@@ -8,7 +8,8 @@ echo "-----------------IMAS DB created------------"
 
 . `pwd`/venv/bin/activate
 # . $envs_dir/21_install_python_packages_for_tutorial.sh
-python -m pip install --ignore-installed ".[docs]"
+
+python -m pip install --no-user --ignore-installed ".[docs]"
 
 cd docs/
 
